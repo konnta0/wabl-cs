247c5007b0597519d7ff1650e37d67b14caef958
konnta0 <konnta0@gmail.com>
2021-12-09 00:55:21 +0900

    WIP: tracing by OTLP

--- dotnet-metric-test/Program.cs
+++ dotnet-metric-test/Program.cs
@@ -1,22 +1,55 @@
 using dotnet_metric_test.APM.Metrics.Counter;
 using dotnet_metric_test.APM.Metrics.Meter;
 using Microsoft.AspNetCore.HttpOverrides;
+using OpenTelemetry.Exporter;
 using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Adding the OtlpExporter creates a GrpcChannel.
+// This switch must be set before creating a GrpcChannel/HttpClient when calling an insecure gRPC service.
+// See: https://docs.microsoft.com/aspnet/core/grpc/troubleshoot#call-insecure-grpc-services-with-net-core-client
+AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+
 builder.Services.AddOpenTelemetryTracing(provider =>
 {
-    provider.SetSampler(new AlwaysOnSampler());
-    provider.AddSource("TEST_PRODUCT");
+    provider.SetResourceBuilder(ResourceBuilder.CreateDefault()
+        .AddService(builder.Configuration.GetValue<string>("Otlp:ServiceName")));
+    provider.AddAspNetCoreInstrumentation(options =>
+    {
+        options.RecordException = true;
+    });
+    provider.AddHttpClientInstrumentation(options =>
+    {
+        options.RecordException = true;
+    });
+    provider.AddOtlpExporter(options =>
+    {
+        options.Endpoint = new Uri(builder.Configuration.GetValue<string>("Otlp:Endpoint"));
+    });
+    provider.AddConsoleExporter();
 });
 
 builder.Services.AddOpenTelemetryMetrics(providerBuilder =>
 {
+    providerBuilder.SetResourceBuilder(ResourceBuilder.CreateDefault()
+        .AddService(builder.Configuration.GetValue<string>("Otlp:ServiceName")));
+    
     providerBuilder.AddMeter(MyMeter.Name);
+    providerBuilder.AddAspNetCoreInstrumentation();
+    providerBuilder.AddHttpClientInstrumentation();
+    
+    providerBuilder.AddOtlpExporter(options =>
+    {
+        options.Endpoint = new Uri(builder.Configuration.GetValue<string>("Otlp:Endpoint"));
+    });
+
     providerBuilder.AddPrometheusExporter(options =>
     {
         options.StartHttpListener = true;
