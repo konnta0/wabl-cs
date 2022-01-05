eb42272b871a58966fd66ecc62b8e83ac703e595
konnta0 <konnta0@gmail.com>
2022-01-06 00:42:44 +0900

    feat: swagger ui

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -2,6 +2,7 @@ using Infrastructure.Context;
 using Infrastructure.Extension;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using UseCase.Extension;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddUseCase(builder.Configuration);
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "DotnetMetricTest",
+        Description = "This is metric test service."
+    });
+});
 
 builder.Services.AddDbContext<EmployeesContext>(optionsBuilder =>
 {
@@ -24,6 +34,8 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseForwardedHeaders(new ForwardedHeadersOptions
