296804ff25391a7a0f50c372b8f7a710a9f5086c
konnta0 <konnta0@gmail.com>
2022-01-28 00:24:08 +0900

    fix: nits

--- src/UseCase/Core/AsyncUseCaseInstrumentationHandler.cs
+++ src/UseCase/Core/AsyncUseCaseInstrumentationHandler.cs
@@ -1,26 +1,43 @@
 using System.Diagnostics;
 using System.Text.Json;
 using Infrastructure.Extension.Instrumentation;
+using Microsoft.Extensions.Logging;
+using ZLogger;
 
 namespace UseCase.Core;
 
 internal class AsyncUseCaseInstrumentationHandler<TInputData, TOutputData> : AsyncInternalUseCaseHandler<TInputData, TOutputData> where TInputData : IInputData where TOutputData : IOutputData
 {
+    private readonly ILogger<AsyncUseCaseInstrumentationHandler<TInputData, TOutputData>> _logger;
+
+    public AsyncUseCaseInstrumentationHandler(
+        ILogger<AsyncUseCaseInstrumentationHandler<TInputData, TOutputData>> logger)
+    {
+        _logger = logger;
+    }
 
     public override async ValueTask<TOutputData> InvokeAsync(TInputData request, CancellationToken cancellationToken, Func<TInputData, CancellationToken, ValueTask<TOutputData>> next)
     {
         using var activity = UseCaseInstrumentationHelper.ActivitySource.StartActivity(
             UseCaseInstrumentationHelper.ActivityName,
-            ActivityKind.Client,
+            ActivityKind.Server,
             Activity.Current?.Context ?? default(ActivityContext)
         );
 
-        var response = await next(request, cancellationToken);
-        if (activity is not null)
+        activity?.SetTag("InputData", request);
+
+        TOutputData response;
+        try
         {
-            activity.SetTag("InputData", request);
-            activity.SetTag("OutputData", response);
+            response = await next(request, cancellationToken);
         }
+        catch (Exception e)
+        {
+            _logger.ZLogError(e.Message);
+            if (e.StackTrace != null) _logger.ZLogError(e.StackTrace);
+            throw;
+        }
+        activity?.SetTag("OutputData", response);
         return response;
     }
 }
