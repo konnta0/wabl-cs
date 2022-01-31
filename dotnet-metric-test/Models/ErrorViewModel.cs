096bfbf5c8b421c019d7147d9386b20d31fee2ec
konnta0 <konnta0@gmail.com>
2022-02-01 00:59:09 +0900

    remove old project

--- dotnet-metric-test/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace dotnet_metric_test.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
