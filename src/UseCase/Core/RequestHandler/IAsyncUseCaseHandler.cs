57e82a53bc7043f46c5083d95559b2ec15d1bc5f
konnta0 <konnta0@gmail.com>
2022-02-04 00:40:59 +0900

    remove: unused

--- src/UseCase/Core/RequestHandler/IAsyncUseCaseHandler.cs
+++
@@ -1,7 +0,0 @@
-using MessagePipe;
-
-namespace UseCase.Core.RequestHandler;
-
-public interface IAyncUseCaseHandler<in TInput, out TOut> where TInput : IInputData where TOut : IOutputData, IRequestHandler<TInput, TOut>
-{
-}
