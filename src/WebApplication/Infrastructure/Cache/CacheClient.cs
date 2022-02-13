c3e6a9cd0f51239e76038fb282df98abd2b60c12
konnta0 <konnta0@gmail.com>
2022-02-14 00:05:02 +0900

    wip CacheClient

--- src/WebApplication/Infrastructure/Cache/CacheClient.cs
+++ src/WebApplication/Infrastructure/Cache/CacheClient.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Microsoft.Extensions.Logging;
 using StackExchange.Redis;
 
@@ -6,16 +7,14 @@ namespace Infrastructure.Cache;
 internal class CacheClient : ICacheClient
 {
     private readonly ILogger<CacheClient> _logger;
-    private readonly ICacheClientFactory _cacheClientFactory;
-    private Lazy<IConnectionMultiplexer> _connectionMultiplexer;
+    private readonly Lazy<IConnectionMultiplexer> _connectionMultiplexer;
 
     public CacheClient(ILogger<CacheClient> logger, ICacheClientFactory cacheClientFactory)
     {
         _logger = logger;
-        _cacheClientFactory = cacheClientFactory;
         _connectionMultiplexer = new Lazy<IConnectionMultiplexer>(() =>
         { 
-            var connectionMultiplexer = _cacheClientFactory.Create();
+            var connectionMultiplexer = cacheClientFactory.Create();
             connectionMultiplexer.ErrorMessage += (sender, args) =>
             {
                 
@@ -34,5 +33,35 @@ internal class CacheClient : ICacheClient
             };
             return connectionMultiplexer;
         });
+    }
+
+    public async Task<bool> KeyExists(string key)
+    {
+        return await _connectionMultiplexer.Value.GetDatabase().KeyExistsAsync(key);
+    }
+
+    public async Task<bool> KeyExpireAsync(string key, TimeSpan? expiry)
+    {
+        return await _connectionMultiplexer.Value.GetDatabase().KeyExpireAsync(key, expiry);
+    }
+
+    public async Task<bool> KeyExpireAsync(string key, DateTime? expiry)
+    {
+        return await _connectionMultiplexer.Value.GetDatabase().KeyExpireAsync(key, expiry);
+    }
+
+    public async Task<bool> KeyPersistAsync(string key)
+    {
+        return await _connectionMultiplexer.Value.GetDatabase().KeyPersistAsync(key);
+    }
+
+    public async Task<bool> HashSetAsync<T>(string key, T value)
+    {
+        return await _connectionMultiplexer.Value.GetDatabase().HashSetAsync(key, nameof(CacheClient), JsonSerializer.Serialize(value));
+    }
+
+    public async Task<bool> KeyDeleteAsync<T>(string key)
+    {
+        return await _connectionMultiplexer.Value.GetDatabase().KeyDeleteAsync(key);
     }
 }
--- src/WebApplication/Infrastructure/Cache/ICacheClient.cs
+++ src/WebApplication/Infrastructure/Cache/ICacheClient.cs
@@ -3,4 +3,10 @@ namespace Infrastructure.Cache;
 
 public interface ICacheClient
 {
+    Task<bool> KeyExists(string key);
+    Task<bool> KeyExpireAsync(string key, TimeSpan? expiry);
+    Task<bool> KeyExpireAsync(string key, DateTime? expiry);
+    Task<bool> KeyPersistAsync(string key);
+    Task<bool> HashSetAsync<T>(string key, T value);
+    Task<bool> KeyDeleteAsync<T>(string key);
 }
