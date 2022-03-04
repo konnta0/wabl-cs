65ae8ee421887dba29b6babc6ccb734150bf6f66
konnta0 <konnta0@gmail.com>
2022-03-05 00:47:12 +0900

    WIP: Controller

--- src/Tool/LoadTest/Controller/Program.cs
+++ src/Tool/LoadTest/Controller/Program.cs
@@ -1,6 +1,17 @@
 ﻿using DFrame;
-using Microsoft.AspNetCore.Builder;
 
-var builder = WebApplication.CreateBuilder(args);
 
-await builder.RunDFrameControllerAsync();
+var parsed = int.TryParse(Environment.GetEnvironmentVariable("PORT_WEB"), out var portWeb);
+if (!parsed)
+{
+    throw new ArgumentException($"Invalid env. PORT_WEB: {Environment.GetEnvironmentVariable("PORT_WEB")}");
+}
+
+parsed = int.TryParse(Environment.GetEnvironmentVariable("PORT_WORKER"), out var portListenWorker);
+if (!parsed)
+{
+    throw new ArgumentException($"Invalid env. PORT_WORKER: {Environment.GetEnvironmentVariable("PORT_WORKER")}");
+}
+
+var builder = DFrameApp.CreateBuilder(portWeb, portListenWorker);
+await builder.RunControllerAsync();
