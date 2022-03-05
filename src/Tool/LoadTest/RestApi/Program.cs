6db7559e1d2aa1f3559e31c65c4542873e3e210b
konnta0 <konnta0@gmail.com>
2022-03-06 01:13:45 +0900

    wip: rest api

---
+++ src/Tool/LoadTest/RestApi/Program.cs
@@ -0,0 +1,21 @@
+﻿using DFrame.RestSdk;
+
+
+var rootAddress = Environment.GetEnvironmentVariable("CONTROLLER_ROOT_ADDRESS");
+if (string.IsNullOrEmpty(rootAddress))
+{
+    throw new ArgumentException($"Invalid env. CONTROLLER_ROOT_ADDRESS: {rootAddress}");
+}
+
+var client = new DFrameClient(rootAddress);
+
+await client.ExecuteRequestAsync(new()
+{
+    Workload = "ListWorkload",
+    Concurrency = 10,
+    TotalRequest = 100000
+});
+
+await client.WaitUntilCanExecute();
+
+var result = await client.GetLatestResultAsync();
