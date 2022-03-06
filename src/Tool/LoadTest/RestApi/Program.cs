7131bd16f9fff203e38f40fcfcb5b732e101508c
konnta0 <konnta0@gmail.com>
2022-03-07 00:18:36 +0900

    read parameter from env

--- src/Tool/LoadTest/RestApi/Program.cs
+++ src/Tool/LoadTest/RestApi/Program.cs
@@ -7,13 +7,31 @@ if (string.IsNullOrEmpty(rootAddress))
     throw new ArgumentException($"Invalid env. CONTROLLER_ROOT_ADDRESS: {rootAddress}");
 }
 
+var workLoadName = Environment.GetEnvironmentVariable("WORKLOAD_NAME");
+if (string.IsNullOrEmpty(workLoadName))
+{
+    throw new ArgumentException($"Invalid env. WORKLOAD_NAME: {workLoadName}");
+}
+
+var parsed = int.TryParse(Environment.GetEnvironmentVariable("CONCURRENCY"), out var concurrency);
+if (!parsed)
+{
+    throw new ArgumentException($"Invalid env. CONCURRENCY: {concurrency}");
+}
+
+parsed = int.TryParse(Environment.GetEnvironmentVariable("TOTAL_REQUEST"), out var totalReqeust);
+if (!parsed)
+{
+    throw new ArgumentException($"Invalid env. TOTAL_REQUEST: {totalReqeust}");
+}
+
 var client = new DFrameClient(rootAddress);
 
 await client.ExecuteRequestAsync(new()
 {
-    Workload = "ListWorkload",
-    Concurrency = 10,
-    TotalRequest = 100000
+    Workload = workLoadName,
+    Concurrency = concurrency,
+    TotalRequest = totalReqeust
 });
 
 await client.WaitUntilCanExecute();
