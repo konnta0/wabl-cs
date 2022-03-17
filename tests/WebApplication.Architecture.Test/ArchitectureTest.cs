f277ac9f4c9ac6d967f61e7c627ccfdb207184bf
konnta0 <konnta0@gmail.com>
2022-03-18 00:47:47 +0900

    feat: Check ShouldNotPresentationDirectoryReferenceRepository

--- tests/WebApplication.Architecture.Test/ArchitectureTest.cs
+++ tests/WebApplication.Architecture.Test/ArchitectureTest.cs
@@ -1,11 +1,26 @@
+using System.Reflection;
+using NetArchTest.Rules;
 using Xunit;
 
 namespace WebApplication.Architecture.Test;
 
 public class ArchitectureTest
 {
-    [Fact]
-    public void Test1()
+    public ArchitectureTest()
     {
+        Assembly.LoadFrom("DotnetMetricTestApp.dll");
+    }
+    
+    [Fact]
+    public void ShouldNotPresentationDirectoryReferenceRepository()
+    {
+        var result = Types.InNamespace("Presentation.Controllers")
+            .That()
+            .ResideInNamespace("Presentation.Controllers")
+            .ShouldNot()
+            .HaveDependencyOn("Infrastructure.Repository")
+            .GetResult()
+            .IsSuccessful;
+        Assert.True(result);
     }
 }
