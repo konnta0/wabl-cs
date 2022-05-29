64ca8e4066384340aa127262d6479613ae54d16f
konnta0 <konnta0@gmail.com>
2022-05-30 00:22:06 +0900

    certmanager

---
+++ src/Tool/Infrastructure/Certificate/CertManager.cs
@@ -0,0 +1,41 @@
+using Microsoft.Extensions.Logging;
+using Pulumi;
+using Pulumi.Kubernetes.Helm.V3;
+using Pulumi.Kubernetes.Types.Inputs.Helm.V3;
+
+namespace Infrastructure.Certificate
+{
+    public class CertManager
+    {
+        private readonly ILogger<CertManager> _logger;
+
+        public CertManager(ILogger<CertManager> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Apply()
+        {
+
+            var certManager = new Release("cert-manager", new ReleaseArgs
+            {
+                Chart = "cert-manager",
+                // helm search repo cert-manager
+                // NAME                                    CHART VERSION   APP VERSION     DESCRIPTION
+                // jetstack/cert-manager                   v1.8.0          v1.8.0          A Helm chart for cert-manager
+                Version = "v1.8.0",
+                RepositoryOpts = new RepositoryOptsArgs
+                {
+                    Repo = "https://charts.jetstack.io"
+                },
+                CreateNamespace = true,
+                Namespace = "certificate",
+                Timeout = 60 * 10,
+                Atomic = true
+            });
+            Namespace = certManager.Namespace;
+        }
+
+        [Output] public Output<string> Namespace { get; private set; }
+    }
+}
