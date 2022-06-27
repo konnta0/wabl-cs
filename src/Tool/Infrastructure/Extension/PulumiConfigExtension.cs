a67ccdf0540fc55c87498578e5fba31defc7bcbc
konnta0 <konnta0@gmail.com>
2022-06-28 01:29:41 +0900

    read from config

--- src/Tool/Infrastructure/Extension/PulumiConfigExtension.cs
+++ src/Tool/Infrastructure/Extension/PulumiConfigExtension.cs
@@ -1,3 +1,4 @@
+using Infrastructure.Observability;
 using Pulumi;
 
 namespace Infrastructure.Extension
@@ -7,6 +8,11 @@ namespace Infrastructure.Extension
         internal static bool IsMinikube(this Config config)
         {
             return config.GetBoolean("isMinikube") ?? false;
+        }
+
+        internal static ObservabilityConfig GetObservabilityConfig(this Config config)
+        {
+            return config.GetObject<ObservabilityConfig>("Observability");
         }
     }
 }
---
+++ src/Tool/Infrastructure/Observability/ObservabilityConfig.cs
@@ -0,0 +1,7 @@
+namespace Infrastructure.Observability
+{
+    public struct ObservabilityConfig
+    {
+        public string Namespace { get; set; }
+    }
+}
