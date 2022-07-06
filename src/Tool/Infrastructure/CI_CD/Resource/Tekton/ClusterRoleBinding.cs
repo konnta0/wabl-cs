7cdf9937a74402029bc1c7adc1edb7873bd4b1b7
konnta0 <konnta0@gmail.com>
2022-07-07 00:42:21 +0900

    add: ClusterRoleBinding

---
+++ src/Tool/Infrastructure/CI_CD/Resource/Tekton/ClusterRoleBinding.cs
@@ -0,0 +1,35 @@
+using System.Collections.Immutable;
+using Pulumi;
+using Pulumi.Kubernetes.Yaml;
+
+namespace Infrastructure.CI_CD.Resource.Tekton
+{
+    public class ClusterRoleBinding
+    {
+        private readonly Config _config;
+
+        public ClusterRoleBinding(Config config)
+        {
+            _config = config;
+        }
+
+        public void Apply()
+        {
+            _ = new ConfigFile("tekton-pipeline-cluster-role-binding", new ConfigFileArgs
+            {
+                File = "./CI_CD/Resource/Tekton/Yaml/ClusterRoleBinding.yaml",
+                Transformations =
+                {
+                    TransformNamespace
+                }
+            });
+        }
+        
+        private ImmutableDictionary<string, object> TransformNamespace(ImmutableDictionary<string, object> obj, CustomResourceOptions opts)
+        {
+            var metadata = (ImmutableDictionary<string, object>)obj["metadata"];
+            if (!metadata.ContainsKey("namespace")) return obj;
+            return obj.SetItem("metadata", metadata.SetItem("namespace", "tekton-pipelines"));
+        }
+    }
+}
