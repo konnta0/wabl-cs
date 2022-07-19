f7588538d39cd42769db68b2ed75d64d282cf42c
konnta0 <konnta0@gmail.com>
2022-07-20 00:05:01 +0900

    pipeline run

---
+++ src/Tool/Infrastructure/CI_CD/Resource/Tekton/PipelineRun.cs
@@ -0,0 +1,35 @@
+using System.Collections.Immutable;
+using Pulumi;
+using Pulumi.Kubernetes.Yaml;
+
+namespace Infrastructure.CI_CD.Resource.Tekton
+{
+    public class PipelineRun
+    {
+        private readonly Config _config;
+
+        public PipelineRun(Config config)
+        {
+            _config = config;
+        }
+
+        public void Apply()
+        {
+            _ = new ConfigFile("tekton-pipeline-build-image", new ConfigFileArgs
+            {
+                File = "./CI_CD/Resource/Tekton/Yaml/PipelineRun/BuildImage.yaml",
+                Transformations =
+                {
+                    TransformNamespace
+                }
+            });
+        }
+
+        private ImmutableDictionary<string, object> TransformNamespace(ImmutableDictionary<string, object> obj, CustomResourceOptions opts)
+        {
+            var metadata = (ImmutableDictionary<string, object>)obj["metadata"];
+            if (!metadata.ContainsKey("namespace")) return obj;
+            return obj.SetItem("metadata", metadata.SetItem("namespace", "tekton-pipelines"));
+        }
+    }
+}
