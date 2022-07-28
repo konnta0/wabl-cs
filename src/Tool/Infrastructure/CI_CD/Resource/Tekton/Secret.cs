3b2d6ebb04d50d808dd59398d3fb9143131cfbbb
konnta0 <konnta0@gmail.com>
2022-07-29 01:59:31 +0900

    use basic auth

--- src/Tool/Infrastructure/CI_CD/Resource/Tekton/Secret.cs
+++ src/Tool/Infrastructure/CI_CD/Resource/Tekton/Secret.cs
@@ -1,6 +1,4 @@
 using System.Collections.Generic;
-using System.Text.Json;
-using Pulumi;
 using Pulumi.Kubernetes.Types.Inputs.Core.V1;
 using Pulumi.Kubernetes.Types.Inputs.Meta.V1;
 
@@ -15,27 +13,24 @@ namespace Infrastructure.CI_CD.Resource.Tekton
 
         public void Apply()
         {
-            var config = new Dictionary<string, object>
-            {
-                ["auths"] = new Dictionary<string, object>
-                {
-                    ["core.harbor.cr.test"] = new Dictionary<string, object>
-                    {
-                    }
-                }
-            };
             _ = new Pulumi.Kubernetes.Core.V1.Secret("tekton-pipeline-secret-container-registry", new SecretArgs
             {
-                Type = "kubernetes.io/dockerconfigjson",
+                Type = "kubernetes.io/basic-auth",
                 Immutable = true,
                 StringData = new Dictionary<string, string>
                 {
-                    [".dockerconfigjson"] = JsonSerializer.Serialize(config)
+                    ["username"] = "admin",
+                    ["password"] = "Harbor12345"
                 },
                 Metadata = new ObjectMetaArgs
                 {
                     Name = "tekton-pipeline-secret-for-container-registry",
-                    Namespace = "tekton-pipelines"
+                    Namespace = "tekton-pipelines",
+                    Annotations = new Dictionary<string, string>
+                    {
+                        // https://tekton.dev/vault/pipelines-v0.16.3/auth/#configuring-basic-auth-authentication-for-docker
+                        ["tekton.dev/docker-0"] = "core.harbor.cr.test"
+                    }
                 }
             });
         }
