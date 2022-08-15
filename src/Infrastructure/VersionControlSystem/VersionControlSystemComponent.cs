70ee5fffc39c78bced817fdfb3fc2ad20f564eb7
konnta0 <konnta0@gmail.com>
2022-08-16 01:58:27 +0900

    create namespace

--- src/Infrastructure/VersionControlSystem/VersionControlSystemComponent.cs
+++ src/Infrastructure/VersionControlSystem/VersionControlSystemComponent.cs
@@ -1,6 +1,10 @@
+using Infrastructure.Extension;
 using Infrastructure.VersionControlSystem.Resource.GiLab;
 using Microsoft.Extensions.Logging;
 using Pulumi;
+using Pulumi.Kubernetes.Core.V1;
+using Pulumi.Kubernetes.Types.Inputs.Core.V1;
+using Pulumi.Kubernetes.Types.Inputs.Meta.V1;
 
 namespace Infrastructure.VersionControlSystem
 {
@@ -9,6 +13,7 @@ namespace Infrastructure.VersionControlSystem
         private Config _config;
         private readonly GitLabResource _gitLabResource;
         private readonly ILogger<VersionControlSystemComponent> _logger;
+        private Input<string> _namespaceName;
 
         public VersionControlSystemComponent(ILogger<VersionControlSystemComponent> logger, Config config, GitLabResource gitLabResource)
         {
@@ -17,9 +22,17 @@ namespace Infrastructure.VersionControlSystem
             _gitLabResource = gitLabResource;
         }
 
-        public void Apply()
+        public Output<string> Apply()
         {
-            _gitLabResource.Apply();
+            var @namespace = new Namespace("namespace-version-control-system", new NamespaceArgs
+            {
+                Metadata = new ObjectMetaArgs
+                {
+                    Name = _config.GetVersionControlSystemConfig().Namespace
+                }
+            });
+            _namespaceName = @namespace.Metadata.Apply(x => x.Name);
+            return _gitLabResource.Apply(_namespaceName);
         }
     }
 }
