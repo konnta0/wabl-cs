424ce167edc523f8d66f02cc771ade6e96da4125
konnta0 <konnta0@gmail.com>
2022-08-19 01:42:01 +0900

    feat: config

--- src/Infrastructure/ContainerRegistry/ContainerRegistryConfig.cs
+++ src/Infrastructure/ContainerRegistry/ContainerRegistryConfig.cs
@@ -3,5 +3,17 @@ namespace Infrastructure.ContainerRegistry
     public struct ContainerRegistryConfig
     {
         public string Namespace { get; set; }
+        public HarborConfig Harbor { get; set; }
+        public MinIOConfig MinIO { get; set; }
+
+        public struct HarborConfig
+        {
+            public NodeSelector NodeSelector { get; set; }
+        }
+
+        public struct MinIOConfig
+        {
+            public NodeSelector NodeSelector { get; set; }
+        }
     }
 }
