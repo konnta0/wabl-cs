7e2db5fd110d51f9b81af81e93e2aea21148c7c1
konnta0 <konnta0@gmail.com>
2022-09-05 00:46:57 +0900

    wip: values

--- src/Infrastructure/WebApplication/Resource/Dragonfly/DragonflyResource.cs
+++ src/Infrastructure/WebApplication/Resource/Dragonfly/DragonflyResource.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
+using System.Linq;
 using Infrastructure.Extension;
 using Pulumi;
 using Pulumi.Kubernetes.Helm.V3;
+using Pulumi.Kubernetes.Types.Inputs.Core.V1;
 using Pulumi.Kubernetes.Types.Inputs.Helm.V3;
+using Pulumi.Kubernetes.Types.Inputs.Meta.V1;
+using Pulumi.Kubernetes.Types.Inputs.Networking.V1;
 
 namespace Infrastructure.WebApplication.Resource.Dragonfly
 {
@@ -16,6 +21,30 @@ namespace Infrastructure.WebApplication.Resource.Dragonfly
 
         public void Apply()
         {
+            // https://github.com/dragonflyoss/helm-charts/blob/main/charts/dragonfly/values.yaml
+            var values = new Dictionary<string, object>
+            {
+                ["externalManager"] = new Dictionary<string, object>
+                {
+                    ["host"] = "dragonfly-manager.dragonfly-system.svc.cluster.local",
+                    ["port"] = 80
+                },
+                ["externalRedis"] = new Dictionary<string, object>
+                {
+                    ["host"] = "redis.dragonfly.webapp.test"
+                },
+                ["mysql"] = new Dictionary<string, object>
+                {
+                    ["auth"] = new Dictionary<string, object>
+                    {
+                        ["host"] = "mysql.dragonfly.webapp.test"
+                    }
+                },
+                ["manager"] = new Dictionary<string, object>
+                {
+                    ["replicas"] = 1
+                }
+            };
             var dragonfly = new Release("web-application-dragonfly", new ReleaseArgs
             {
                 Chart = "dragonfly",
@@ -30,6 +59,7 @@ namespace Infrastructure.WebApplication.Resource.Dragonfly
                 {
                     Repo = "https://dragonflyoss.github.io/helm-charts"
                 },
+                Values = values,
                 CreateNamespace = false,
                 Atomic = true,
                 Namespace = _config.GetWebApplicationConfig().Namespace
