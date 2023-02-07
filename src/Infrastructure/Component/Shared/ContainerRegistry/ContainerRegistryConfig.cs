8702c278f8601d0f3f35cd1b4defeae678a9df12
konnta0 <konnta0@gmail.com>
2023-02-08 01:05:57 +0900

    refactor: remove unused

--- src/Infrastructure/Component/Shared/ContainerRegistry/ContainerRegistryConfig.cs
+++ src/Infrastructure/Component/Shared/ContainerRegistry/ContainerRegistryConfig.cs
@@ -3,18 +3,12 @@ namespace Infrastructure.Component.Shared.ContainerRegistry
     public struct ContainerRegistryConfig
     {
         public HarborConfig Harbor { get; set; }
-        public MinIOConfig MinIO { get; set; }
 
         public struct HarborConfig
         {
             public NodeSelector NodeSelector { get; set; }
         }
 
-        public struct MinIOConfig
-        {
-            public NodeSelector NodeSelector { get; set; }
-        }
-        
         public string Host { get; set; }
     }
 }
