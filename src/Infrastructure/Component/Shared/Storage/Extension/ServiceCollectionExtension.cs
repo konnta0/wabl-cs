cc9f8b3222962ca14b04fd78b9e1d95d97cdda01
konnta0 <konnta0@gmail.com>
2023-02-10 00:23:55 +0900

    feat: add DI container

--- src/Infrastructure/Component/Shared/Storage/Extension/ServiceCollectionExtension.cs
+++ src/Infrastructure/Component/Shared/Storage/Extension/ServiceCollectionExtension.cs
@@ -1,5 +1,6 @@
 using Infrastructure.Component.Shared.Storage.Dragonfly;
 using Infrastructure.Component.Shared.Storage.MinIo;
+using Infrastructure.Component.Shared.Storage.TiDB;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure.Component.Shared.Storage.Extension
@@ -8,6 +9,7 @@ namespace Infrastructure.Component.Shared.Storage.Extension
     {
         internal static IServiceCollection AddStorageComponent(this IServiceCollection serviceCollection)
         {
+            serviceCollection.AddScoped<TiDBComponent>();
             serviceCollection.AddScoped<DragonflyComponent>();
             serviceCollection.AddScoped<MinIoComponent>();
             serviceCollection.AddScoped<StorageComponent>();
