1dd572cc1721f5d91b61b0260c9825429b06a86b
konnta0 <konnta0@gmail.com>
2023-02-17 00:47:39 +0900

    nits

--- src/Infrastructure/Component/WebApplication/Promtail/PromtailComponentApplyArgs.cs
+++ src/Infrastructure/Component/WebApplication/Promtail/PromtailComponentApplyArgs.cs
@@ -4,7 +4,7 @@ namespace Infrastructure.Component.WebApplication.Promtail
 {
     public sealed class PromtailComponentInput : IComponentInput
     {
-        public Namespace Namespace { get; set; }
+        public Namespace Namespace { get; set; } = null!;
     }
 
     public sealed class PromtailComponentOutput : IComponentOutput
