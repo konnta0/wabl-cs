8f78af3046e042aa4984794eb87dc6e50be3c6a0
konnta0 <konnta0@gmail.com>
2023-03-05 01:45:59 +0900

    feat: swich doragonfly and redis

--- src/Infrastructure/Component/Shared/Storage/StorageComponent.cs
+++ src/Infrastructure/Component/Shared/Storage/StorageComponent.cs
@@ -1,19 +1,30 @@
+using System.Text.Json;
 using Infrastructure.Component.Shared.Storage.Dragonfly;
 using Infrastructure.Component.Shared.Storage.MinIo;
+using Infrastructure.Component.Shared.Storage.Redis;
 using Infrastructure.Component.Shared.Storage.TiDB;
+using Pulumi;
 
 namespace Infrastructure.Component.Shared.Storage
 {
     public class StorageComponent : IComponent<StorageComponentInput, StorageComponentOutput>
     {
+        private readonly Config _config;
         private readonly MinIoComponent _minIoComponent;
         private readonly DragonflyComponent _dragonflyComponent;
+        private readonly RedisComponent _redisComponent;
         private readonly TiDBComponent _tiDbComponent;
 
-        public StorageComponent(MinIoComponent minIoComponent, DragonflyComponent dragonflyComponent, TiDBComponent tiDbComponent)
+        public StorageComponent(Config config, 
+            MinIoComponent minIoComponent, 
+            DragonflyComponent dragonflyComponent, 
+            RedisComponent redisComponent,
+            TiDBComponent tiDbComponent)
         {
+            _config = config;
             _minIoComponent = minIoComponent;
             _dragonflyComponent = dragonflyComponent;
+            _redisComponent = redisComponent;
             _tiDbComponent = tiDbComponent;
         }
 
@@ -23,10 +34,24 @@ namespace Infrastructure.Component.Shared.Storage
             {
                 Namespace = input.Namespace
             });
-            _dragonflyComponent.Apply(new DragonflyComponentInput
+
+            var cache = _config.RequireObject<JsonElement>("Storage").GetProperty("Cache").GetString();
+
+            switch (cache!.ToLower())
             {
-                Namespace = input.Namespace
-            });
+                case "dragonfly":
+                    _dragonflyComponent.Apply(new DragonflyComponentInput
+                    {
+                        Namespace = input.Namespace
+                    });
+                    break;
+                case "redis":
+                    _redisComponent.Apply(new RedisComponentInput
+                    {
+                        Namespace = input.Namespace
+                    });
+                    break;
+            }
             _tiDbComponent.Apply(new TiDBComponentInput
             {
                 Namespace = input.Namespace
