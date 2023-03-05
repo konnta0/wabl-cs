a3db4bedaf49b7ace62993bb1954905f70189083
konnta0 <konnta0@gmail.com>
2023-03-06 00:27:16 +0900

    fix: set global logger

--- src/Tool/DatabaseMigration/Program.cs
+++ src/Tool/DatabaseMigration/Program.cs
@@ -1,11 +1,30 @@
+using Cysharp.Text;
 using DatabaseMigration;
 using DatabaseMigration.Command;
+using Infrastructure.Core.Logging;
 using Infrastructure.Extension;
 using MessagePipe;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ZLogger;
+using ZLogger.Providers;
 
 Console.WriteLine("Start Database migration");
 var builder = ConsoleApp.CreateBuilder(args);
+GlobalLogManager.SetLoggerFactory(LoggerFactory.Create(loggingBuilder =>
+{
+    loggingBuilder.ClearProviders();
+    loggingBuilder.SetMinimumLevel(LogLevel.Information);
+    loggingBuilder.AddFilter<ZLoggerConsoleLoggerProvider>("Microsoft", LogLevel.None);
+    loggingBuilder.AddZLoggerConsole(options =>
+    {
+        options.EnableStructuredLogging = true;
+        var prefixFormat = ZString.PrepareUtf8<LogLevel, DateTime>("[{0}][{1}] ");
+        options.PrefixFormatter = (writer, info) =>
+            prefixFormat.FormatTo(ref writer, info.LogLevel, info.Timestamp.DateTime.ToLocalTime());
+    });
+}), "Global");
+
 builder.ConfigureServices((_, collection) =>
 {
     collection.AddDbContext();
