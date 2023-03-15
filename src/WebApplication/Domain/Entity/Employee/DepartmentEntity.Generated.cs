d201f43f283d4490e32e7fd6cdf66ae86a22f846
konnta0 <konnta0@gmail.com>
2023-03-16 00:22:50 +0900

    chore: change has seed table

--- src/WebApplication/Domain/Entity/Employee/DepartmentEntity.Generated.cs
+++ src/WebApplication/Domain/Entity/Employee/DepartmentEntity.Generated.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Domain.Entity.Employee;
 
-public partial class DepartmentEntity : IEntity, IHasSeed
+public partial class DepartmentEntity : IEntity
 {
     public static partial void OnModelCreating(EntityTypeBuilder<DepartmentEntity> entityTypeBuilder);
 }
--- src/WebApplication/Domain/Entity/Employee/TitlesEntity.Generated.cs
+++ src/WebApplication/Domain/Entity/Employee/TitlesEntity.Generated.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Domain.Entity.Employee;
 
-public partial class TitlesEntity : IEntity
+public partial class TitlesEntity : IEntity, IHasSeed
 {
     public static partial void OnModelCreating(EntityTypeBuilder<TitlesEntity> entityTypeBuilder);
 }
