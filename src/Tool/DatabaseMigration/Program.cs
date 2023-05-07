a90b444f3fa5d400050cc3c70a91a7e84bf3b16c
konnta0 <konnta0@gmail.com>
2023-05-08 00:14:15 +0900

    use config

--- src/Tool/DatabaseMigration/Program.cs
+++ src/Tool/DatabaseMigration/Program.cs
@@ -1,34 +1,18 @@
-using Cysharp.Text;
 using DatabaseMigration;
 using DatabaseMigration.Command;
-using Infrastructure.Core.Logging;
+using Infrastructure.Cache;
+using Infrastructure.Database;
 using Infrastructure.Extension;
 using MessagePipe;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using ZLogger;
-using ZLogger.Providers;
 
 Console.WriteLine("Start Database migration");
 var builder = ConsoleApp.CreateBuilder(args);
-GlobalLogManager.SetLoggerFactory(LoggerFactory.Create(loggingBuilder =>
-{
-    loggingBuilder.ClearProviders();
-    loggingBuilder.SetMinimumLevel(LogLevel.Information);
-    loggingBuilder.AddFilter<ZLoggerConsoleLoggerProvider>("Microsoft", LogLevel.None);
-    loggingBuilder.AddZLoggerConsole(options =>
-    {
-        options.EnableStructuredLogging = true;
-        var prefixFormat = ZString.PrepareUtf8<LogLevel, DateTime>("[{0}][{1}] ");
-        options.PrefixFormatter = (writer, info) =>
-            prefixFormat.FormatTo(ref writer, info.LogLevel, info.Timestamp.DateTime.ToLocalTime());
-    });
-}), "Global");
 
 builder.ConfigureServices((_, collection) =>
 {
-    collection.AddDbContexts();
-    collection.AddCacheClient();
+    collection.AddDbContexts(new DatabaseConfig());
+    collection.AddCacheClient(new CacheConfig(), out var _);
     collection.AddSingleton<IDbContextHolder, DbContextHolder>();
     collection.AddMessagePipe();
 });
