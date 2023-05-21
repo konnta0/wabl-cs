69df07e4d2b10ac2b7a50d56e733e3ba7c6bf093
konnta0 <konnta0@gmail.com>
2023-05-22 00:26:27 +0900

    nits

--- src/Tool/DatabaseMigration/Command/SeedServiceConfig.cs
+++ src/Tool/DatabaseMigration/Command/SeedServiceConfig.cs
@@ -1,10 +1,12 @@
-namespace DatabaseMigration.Command.SeedCreate;
+namespace DatabaseMigration.Command;
 
-public class SeedCreateConfig
+public class SeedServiceConfig
 {
-    public string ApplicationName => "SeedCreate";
+    public string ApplicationName => nameof(SeedService);
     public string SpreadsheetFolderId => Environment.GetEnvironmentVariable("SPREADSHEET_FOLDER_ID") ?? throw new ArgumentNullException(nameof(TemplateSpreadsheetId));
     public string TemplateSpreadsheetId { get; init; } = Environment.GetEnvironmentVariable("TEMPLATE_SPREADSHEET_ID") ?? throw new ArgumentNullException(nameof(TemplateSpreadsheetId));
     public SpreadsheetCell TitleCell => new("A2");
     public SpreadsheetCell DescriptionCell => new("B2");
+    public SpreadsheetCell RowLabelStartCell => new("A7");
+    public SpreadsheetCell ColumnLabelStartCell => new("B6");
 }
