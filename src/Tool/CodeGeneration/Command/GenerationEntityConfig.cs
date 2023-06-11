65b83e2f0ea9236f2fed21f0abe20aa19444f902
konnta0 <konnta0@gmail.com>
2023-06-12 00:03:20 +0900

    add entityname

--- src/Tool/CodeGeneration/Command/GenerationEntityConfig.cs
+++ src/Tool/CodeGeneration/Command/GenerationEntityConfig.cs
@@ -3,5 +3,5 @@ namespace CodeGeneration.Command;
 public class GenerationEntityConfig
 { 
     public string NamespaceName { get; init; } = "Domain.Entity";
-
+    public string EntityName { get; init; }
 }
