99294de172c992a2b90445673fc9fcc8e584d6ae
konnta0 <konnta0@gmail.com>
2023-06-20 00:43:53 +0900

    for parallel execute test

---
+++ tests/WebApplication.Test/TestContainersExample2.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Bogus;
+using Domain.Entity.Employee;
+using FluentAssertions;
+using Infrastructure.Cache;
+using Infrastructure.Database.Context.Employee;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Infrastructure.Extension;
+using Xunit;
+
+namespace WebApplication.Test;
+
+public class TestContainersExample2 : TestBase
+{
+    private IServiceProvider? _serviceProvider;
+
+    protected override async Task SetUpAsync()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection
+            .AddLogging()
+            .AddDbContexts(DatabaseConfig!)
+            .AddCacheClient(CacheConfig!, out _);
+
+        _serviceProvider = serviceCollection.BuildServiceProvider();
+        var employeesContext = _serviceProvider!.GetRequiredService<EmployeesContext>();
+        await employeesContext.Database.MigrateAsync();
+        await employeesContext.Database.EnsureCreatedAsync();
+    }
+
+    [Fact]
+    public async Task UseDatabaseContainer2()
+    {
+        var employeesContext = _serviceProvider!.GetRequiredService<EmployeesContext>();
+        
+        var testTitleEntities = new Faker<TitlesEntity>()
+            .RuleFor(x => x.EmpNo, f => f.Random.Int(1, 1000000))
+            .RuleFor(x => x.Title, f => f.Random.Guid().ToString())
+            .RuleFor(x => x.FromDate, f => f.Date.Past())
+            .RuleFor(x => x.ToDate, f => f.Date.Future());
+
+        var generated = testTitleEntities.Generate(100);
+        await employeesContext.TitlesEntities.AddRangeAsync(generated);
+
+        var faker = new Faker("ja");
+        var employeeEntities = new List<EmployeesEntity>();
+        foreach (var titleEntity in generated)
+        {
+            employeeEntities.Add(new EmployeesEntity
+            {
+                EmpNo = titleEntity.EmpNo,
+                BirthDate = faker.Date.Past(),
+                FirstName = faker.Name.FirstName(),
+                LastName = faker.Name.LastName(),
+                Gender = faker.Random.Enum<EmployeesEntity.GenderType>(),
+                HireDate = faker.Date.Past()
+            });
+        }
+        await employeesContext.EmployeesEntities.AddRangeAsync(employeeEntities);
+        
+        await employeesContext.SaveChangesAsync();
+        
+        var newTitleEntities = await employeesContext.TitlesEntities.ToListAsync();
+        newTitleEntities.Should().HaveCount(100);
+    }
+
+    [Fact]
+    public async Task UseCacheContainer2()
+    {
+        const string key = "test";
+
+        var cacheClient = _serviceProvider!.GetRequiredService<IVolatileCacheClient>();
+        var keyExists = await cacheClient.KeyExistsAsync(key);
+        keyExists.Should().BeFalse();
+
+        var setAddResult = await cacheClient.SetAddAsync(key, "value");
+        setAddResult.Should().BeTrue();
+
+        var keyExistsAfterSet = await cacheClient.KeyExistsAsync(key);
+        keyExistsAfterSet.Should().BeTrue();
+    }
+}
