6b62dacac9e435e31047537dcc943be2ba647855
konnta0 <konnta0@gmail.com>
2023-06-22 00:05:47 +0900

    feat: add  debug project for source generator

---
+++ src/Tool/ForSouceGeneratorDebug/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Domain.Entity.Employee;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+var mc = new ExampleEntity();
+Console.WriteLine(mc);
+
+
+
+[Table("Example")]
+public partial class ExampleEntity
+{
+    public int Fuga { get; set; }
+    public string? Piyo { get; set; }
+
+    public static partial void OnModelCreating(EntityTypeBuilder<ExampleEntity> entityTypeBuilder)
+    {
+        entityTypeBuilder.HasOne<EmployeesEntity>();
+    }
+}
+
+[Table("Hoge")]
+public partial class HogeEntity
+{
+    public int Fuga { get; set; }
+    public string? Piyo { get; set; }
+
+    public static partial void OnModelCreating(EntityTypeBuilder<HogeEntity> entityTypeBuilder)
+    {
+        entityTypeBuilder.HasOne<EmployeesEntity>();
+    }
+}
