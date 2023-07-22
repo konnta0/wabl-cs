3f4f0394d2dbf46a0dc9743b02293465f2ab9346
konnta0 <konnta0@gmail.com>
2023-07-22 12:04:29 +0900

    renam

--- src/WebApplication/Presentation/Extension/ResponseDataFactory/Departments/ListResponseFactory.cs
+++ src/WebApplication/Presentation/Extension/ResponseDataFactory/Departments/ListResponseFactory.cs
@@ -1,15 +1,14 @@
 using Domain.RestApi.Departments;
 using Domain.RestApi.Departments.Object;
 using UseCase.Departments;
-using UseCase.Departments.List;
 
 namespace Presentation.Extension.ResponseDataFactory.Departments;
 
 internal static class ListResponseDataFactory
 {
-    public static ListResponseData Create(ListDepartmentsUseCaseOutput useCaseOutput)
+    public static ListResponse Create(ListDepartmentsUseCaseOutput useCaseOutput)
     {
-        return new ListResponseData
+        return new ListResponse
         {
             Departments =
                 useCaseOutput.Departments?.Select(x => new Department { DepotNo = x.DepotNo, DeptName = x.DeptName })
