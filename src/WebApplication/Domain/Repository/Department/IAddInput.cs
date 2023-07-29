8cc5f0e3909c7e3966b8d5ba83c328749f44ff7b
konnta0 <konnta0@gmail.com>
2023-07-30 01:32:54 +0900

    fix

--- src/WebApplication/Domain/Repository/Department/IAddInput.cs
+++ src/WebApplication/Domain/Repository/Department/IAddInput.cs
@@ -2,5 +2,6 @@ namespace Domain.Repository.Department;
 
 public interface IAddInput : ITransactionalInput, IDepartmentRepositoryInput
 {
-    
+    public string DepotNo { get; init; }
+    public string DeptName { get; init; }
 }
--- src/WebApplication/Infrastructure/Repository/Department/AddInput.cs
+++ src/WebApplication/Infrastructure/Repository/Department/AddInput.cs
@@ -5,4 +5,6 @@ namespace Infrastructure.Repository.Department;
 public class AddInput : IAddInput
 {
     public bool UseTransaction { get; init; } = true;
+    public string DepotNo { get; init; } = string.Empty;
+    public string DeptName { get; init; } = string.Empty;
 }
