d18e63d2ce284e705bcd57764588059903263841
konnta0 <konnta0@gmail.com>
2023-09-20 00:26:05 +0900

    refactor

--- src/WebApplication/UseCase/Extension/ServiceCollection.cs
+++ src/WebApplication/UseCase/Extension/ServiceCollection.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using UseCase.Core.BackgroundService;
 using UseCase.Core.RequestHandler;
 
 namespace UseCase.Extension;
@@ -9,8 +10,20 @@ public static class ServiceCollection
     public static IServiceCollection AddUseCase(this IServiceCollection serviceCollection,
         IConfiguration configuration)
     {
-
+        serviceCollection.AddUseCaseHandler();
+        serviceCollection.AddBackgroundService();
+        return serviceCollection;
+    }
+    
+    private static IServiceCollection AddUseCaseHandler(this IServiceCollection serviceCollection)
+    {
         serviceCollection.AddScoped<IUseCaseHandler, UseCaseHandler>();
+        return serviceCollection;
+    }
+    
+    private static IServiceCollection AddBackgroundService(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddHostedService<MemoryDatabaseLoader>();
         return serviceCollection;
     }
 }
