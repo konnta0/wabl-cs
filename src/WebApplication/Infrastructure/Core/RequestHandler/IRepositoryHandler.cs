a10eca17feca108f430f4ddb9cd0c0ce28b675b0
konnta0 <konnta0@gmail.com>
2023-09-26 00:05:28 +0900

    chore: move

--- src/WebApplication/Infrastructure/Core/RequestHandler/IRepositoryHandler.cs
+++
@@ -1,9 +0,0 @@
-using Domain.Repository;
-
-namespace Infrastructure.Core.RequestHandler;
-
-public interface IRepositoryHandler
-{
-    ValueTask<TOutput> InvokeAsync<TInput, TOutput>(TInput input, CancellationToken cancellationToken = new())
-        where TInput : IRepositoryInput where TOutput : IRepositoryOutput;
-}
