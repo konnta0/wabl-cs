70adc5ca7fb8c9adacada8b75de9fe69272358ad
konnta0 <konnta0@gmail.com>
2023-09-29 00:18:16 +0900

    chore: arch test

--- tests/WebApplication.Architecture.Test/ArchitectureTest.cs
+++ tests/WebApplication.Architecture.Test/ArchitectureTest.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using NetArchTest.Rules;
+using NetArchTest.Rules.Policies;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -28,5 +30,52 @@ public class ArchitectureTest
         _testOutputHelper.WriteLine("Failure type names is ... " + string.Join("\n", result.FailingTypeNames ?? new List<string?>{"Nothing!!"}));
 
         Assert.True(result.IsSuccessful);
+    }
+
+    [Fact]
+    public void ShouldNotPresentationDirectoryReferenceInfrastructure()
+    {
+        var result = Types.InAssembly(typeof(Presentation.Extension.ServiceCollectionExtension).Assembly)
+            .That()
+            .ResideInNamespaceStartingWith("Presentation")
+            .ShouldNot()
+            .HaveDependencyOn("Infrastructure")
+            .GetResult();
+        _testOutputHelper.WriteLine("Failure type names is ... " + string.Join("\n", result.FailingTypeNames ?? new List<string?>{"Nothing!!"}));
+
+        Assert.True(result.IsSuccessful);
+    }
+    
+    [Fact]
+    public void InterfaceNamingPolicy()
+    {
+        var assemblies = new List<Assembly>
+        {
+            typeof(Application.Extension.ServiceCollectionExtension).Assembly,
+            typeof(Domain.Entity.IHasSeed).Assembly,
+            typeof(Domain.RestApi.IApi<,>).Assembly,
+            typeof(Infrastructure.Extension.ServiceCollectionExtension).Assembly,
+            typeof(Presentation.Extension.ServiceCollectionExtension).Assembly,
+            typeof(Presentation.BackgroundService.Extension.ServiceCollectionExtension).Assembly,
+        };
+
+        var architecturePolicy = Policy
+            .Define("InterfaceNamingPolicy", "Interface name should start with I")
+            .For(Types.InAssemblies(assemblies))
+            .Add(t =>
+                    t.That()
+                        .AreInterfaces()
+                        .Should()
+                        .HaveNameStartingWith("I"),
+                "Generic implementation rules", "Interface names should start with an 'I'"
+            );
+        var results = architecturePolicy.Evaluate();
+        results.Results
+            .Where(static x => x.FailingTypes is not null)
+            .Select(static x => x.FailingTypes)
+            .ToList()
+            .ForEach(x => _testOutputHelper.WriteLine("Failure type names is ... " + string.Join("\n", x)));
+
+        Assert.False(results.HasViolations);
     }
 }
