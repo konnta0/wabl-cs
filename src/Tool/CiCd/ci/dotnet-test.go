ed0d1cb8742627ffc78c52b0eed15b256544ee97
konnta0 <konnta0@gmail.com>
2023-10-17 00:17:47 +0900

    dotnet test by dagger

---
+++ src/Tool/CiCd/ci/dotnet-test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"dagger.io/dagger"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// initialize Dagger client
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	golang := client.
+		Container().
+		From("mcr.microsoft.com/dotnet/sdk:7.0.305-alpine3.18").
+		WithExec([]string{"dotnet", "test", "--no-restore", "--logger", "\"trx;LogFileName=test-results.trx\""})
+
+	_, err = golang.Stdout(ctx)
+	if err != nil {
+		panic(err)
+	}
+}
