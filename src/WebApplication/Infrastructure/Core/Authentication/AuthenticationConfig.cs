1b4c510f55248f9431051fd2d9f3c15827974b7f
konnta0 <konnta0@gmail.com>
2023-11-04 00:17:43 +0900

    wip: authentication provider

--- src/WebApplication/Infrastructure/Core/Authentication/AuthenticationConfig.cs
+++ src/WebApplication/Infrastructure/Core/Authentication/AuthenticationConfig.cs
@@ -2,7 +2,12 @@ namespace Infrastructure.Core.Authentication;
 
 public sealed class AuthenticationConfig
 {
+    public string AuthorityBaseUrl { get; init; } = Environment.GetEnvironmentVariable("AUTHENTICATION_BASE_URL")!;
+    public string Realm { get; init; } = Environment.GetEnvironmentVariable("AUTHENTICATION_REALM")!;
     public string Issuer { get; init; } = Environment.GetEnvironmentVariable("AUTHENTICATION_ISSUER")!;
     public string Audience { get; init; } = Environment.GetEnvironmentVariable("AUTHENTICATION_AUDIENCE")!;
     public string Secret { get; init; } = Environment.GetEnvironmentVariable("AUTHENTICATION_SECRET")!;
+    public string AdminUserName { get; init; } = Environment.GetEnvironmentVariable("AUTHENTICATION_ADMIN_USERNAME")!;
+    public string AdminPassword { get; init; } = Environment.GetEnvironmentVariable("AUTHENTICATION_ADMIN_PASSWORD")!;
+    public string AdminBaseUrl { get; init; } = Environment.GetEnvironmentVariable("AUTHENTICATION_ADMIN_BASE_URL")!;
 }
