fbdb2ca48c596db86ccafdddcf40e9bb5d0c9fbd
konnta0 <konnta0@gmail.com>
2023-11-11 10:28:31 +0900

    feat: directory util

---
+++ src/Tool/Make/Common/DirectoryUtil.cs
@@ -0,0 +1,20 @@
+namespace Make.Common;
+
+public static class DirectoryUtil
+{
+    public static bool TryGetSolutionDirectoryInfo(out DirectoryInfo directoryInfo, string? currentPath = null)
+    {
+        var directory = new DirectoryInfo(
+            currentPath ?? Directory.GetCurrentDirectory());
+        directoryInfo = directory;
+        while (directory != null && !directory.GetFiles("*.sln").Any())
+        {
+            directory = directory.Parent;
+        }
+
+        if (directory is null) return false;
+        directoryInfo = directory;
+
+        return true;
+    }
+}
