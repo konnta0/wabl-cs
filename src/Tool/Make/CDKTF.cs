9a237b081cc91e53956780c907800d0cfa77dc49
konnta0 <konnta0@gmail.com>
2023-12-21 00:22:32 +0900

    with install terraform

--- src/Tool/Make/CDKTF.cs
+++ src/Tool/Make/CDKTF.cs
@@ -7,7 +7,9 @@ public sealed class CDKTF : ConsoleAppBase
     public CDKTF()
     {
         _target = new Targets();
-        _target.Add("install-node", () => RunAsync("brew", "install node"));
+        _target.Add("tap-terraform", () => RunAsync("brew", "tap hashicorp/tap"));
+        _target.Add("install-terraform", DependsOn("tap-terraform"), () => RunAsync("brew", "install hashicorp/tap/terraform"));
+        _target.Add("install-node", DependsOn("install-terraform"),  () => RunAsync("brew", "install node"));
         _target.Add("install", DependsOn("install-node"), () => RunAsync("npm", "install -g cdktf-cli"));
     }
 
