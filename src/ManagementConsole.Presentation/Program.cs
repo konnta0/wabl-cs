4b5e01870f66cdbe2b588836fbee3dfad4b71e2f
konnta0 <konnta0@gmail.com>
2023-12-28 00:28:23 +0900

    recreate

--- src/ManagementConsole.Presentation/Program.cs
+++ src/ManagementConsole.Presentation/Program.cs
@@ -1,3 +1,4 @@
+using ManagementConsole.Components;
 using ManagementConsole.Internals;
 using ManagementConsole.Internals.Extension;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
@@ -6,8 +7,8 @@ using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 builder.Services.AddFluentUIComponents();
 builder.Services.AddDataGridEntityFrameworkAdapter();
 builder.Services.AddHttpClient();
@@ -22,7 +23,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -30,12 +31,11 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+app.UseAntiforgery();
 
-app.UseRouting();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
-app.MapBlazorHub();
-app.MapControllers();
-app.MapFallbackToPage("/_Host");
 app.MapHealthChecks("/healthz", new HealthCheckOptions
 {
     AllowCachingResponses = false,
--- src/ManagementConsole.Presentation/entrypoint.sh
+++ src/ManagementConsole.Presentation/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 /usr/local/bin/jetbrains_debugger_agent -port 5033 -login debuguser -password debuguser &
 
-dotnet /app/ManagementConsole.dll
+dotnet /app/ManagementConsole.Presentation.dll
