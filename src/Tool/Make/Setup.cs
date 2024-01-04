1daf6c4ad7fc856d083add54831d0c46129f31f3
konnta0 <konnta0@gmail.com>
2024-01-05 00:42:37 +0900

    setup microk8s

--- src/Tool/Make/Setup.cs
+++ src/Tool/Make/Setup.cs
@@ -13,6 +13,18 @@ public sealed class Setup : ConsoleAppBase
         _target.Add("minikube-version", DependsOn("minikube-version"), static () => RunAsync("minikube", "version"));
         _target.Add("minikube-remove", DependsOn("minikube-remove"), static () => RunAsync("rm", "-f minikube-darwin-amd64"));
 
+        _target.Add("microk8s-brew-install", static () => RunAsync("brew", "install ubuntu/microk8s/microk8s"));
+        _target.Add("microk8s-install", DependsOn("microk8s-brew-install"), static () => RunAsync("microk8s", "install --mem=16 --cpu=7"));
+        _target.Add("microk8s-enable-plugins", DependsOn("microk8s-install"), async () =>
+        {
+             await "microk8s enable dashboard";
+             await "microk8s enable dns";
+             await "microk8s enable ingress";
+             await "microk8s enable registry";
+        });
+
+        _target.Add("microk8s-notice", static () => Console.WriteLine("Please run `microk8s kubectl config view --raw >> ~/.kube/config`"));
+        
         _target.Add("pulumi-curl", static () => RunAsync("curl", "-fsSL https://get.pulumi.com | sh"));
         _target.Add("pulumi-version", DependsOn("pulumi-curl"), static () => RunAsync("pulumi", "version"));
         _target.Add("pulumi-crd2pulumi", DependsOn("pulumi-version"), static () => RunAsync("brew", "install pulumi/tap/crd2pulumi"));
@@ -20,6 +32,13 @@ public sealed class Setup : ConsoleAppBase
 
     [Command("minikube")]
     public Task Minikube() => _target.RunWithoutExitingAsync(["minikube-remove"]);
+
+    [Command("microk8s")]
+    public async Task Microk8s()
+    {
+        await _target.RunWithoutExitingAsync(["microk8s-enable-plugins"]);
+        await _target.RunWithoutExitingAsync(["microk8s-notice"]);
+    }
 
     [Command("pulumi")]
     public Task Pulumi() => _target.RunWithoutExitingAsync(["pulumi-crd2pulumi"]);
