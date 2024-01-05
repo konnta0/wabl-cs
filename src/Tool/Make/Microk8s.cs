d649c63b17c9d9a71ef109854c14caf165509a22
konnta0 <konnta0@gmail.com>
2024-01-06 01:57:44 +0900

    for microk8s command

---
+++ src/Tool/Make/Microk8s.cs
@@ -0,0 +1,11 @@
+namespace Make;
+
+internal sealed class Microk8s : ConsoleAppBase
+{
+    public async ValueTask Stop()
+    {
+        var target = new Targets();
+        target.Add("stop", static () => RunAsync("microk8s", "stop"));
+        await target.RunWithoutExitingAsync(["stop"]);
+    }
+}
