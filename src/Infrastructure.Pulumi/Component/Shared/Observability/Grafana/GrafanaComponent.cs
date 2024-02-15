c19ab03b92f3014a8f2211018a313ec164d6d53f
konnta0 <konnta0@gmail.com>
2024-02-16 02:56:01 +0900

    chore

--- src/Infrastructure.Pulumi/Component/Shared/Observability/Grafana/GrafanaComponent.cs
+++ src/Infrastructure.Pulumi/Component/Shared/Observability/Grafana/GrafanaComponent.cs
@@ -27,163 +27,6 @@ namespace Infrastructure.Pulumi.Component.Shared.Observability.Grafana
 
         public GrafanaComponentOutput Apply(GrafanaComponentInput input)
         {
-            // ref: https://github.com/grafana/helm-charts/blob/main/charts/grafana/values.yaml
-            var namespaceName = string.Empty; 
-            input.Namespace.Metadata.Apply(x =>
-            {
-                namespaceName = x.Name;
-                return x;
-            });
-            var values = new Dictionary<string, object>
-            {
-                ["adminPassword"] = "grafana12345",
-                ["sidecar"] = new Dictionary<string, object>
-                {
-                    ["dashboards"] = new Dictionary<string, object>
-                    {
-                        ["enabled"] = true,
-                        ["label"] = "grafana_dashboard",
-                        ["labelValue"] = bool.TrueString.ToLower(),
-                        ["searchNamespace"] =
-                            string.Join(",", namespaceName, _config.GetWebApplicationConfig().Namespace)
-                    }
-                },
-                ["dashboardProviders"] = new Dictionary<string, object>
-                {
-                    ["tidb-dashboard.yaml"] = new Dictionary<string, object>
-                    {
-                        ["apiVersion"] = 1,
-                        ["providers"] = new List<object>
-                        {
-                            new Dictionary<string, object>
-                            {
-                                ["folder"] = "",
-                                ["name"] = "0",
-                                ["options"] = new Dictionary<string, object>
-                                {
-                                    ["path"] = "/grafana-dashboard-definitions/tidb"
-                                },
-                                ["allowUiUpdates"] = bool.TrueString.ToLower(),
-                                ["orgId"] = 1,
-                                ["type"] = "file"
-                            }
-                        }
-                    }
-                },
-                ["extraVolumeMounts"] = new List<object>
-                {
-                    new Dictionary<string, object>
-                    {
-                        ["name"] = "tidb-grafana-dashboard",
-                        ["mountPath"] = "/grafana-dashboard-definitions/tidb"
-                    }
-                },
-                ["datasources"] = new Dictionary<string, object>
-                {
-                    ["datasource.yaml"] = new Dictionary<string, object>
-                    {
-                        ["apiVersion"] = 1,
-                        ["datasources"] = new List<object>
-                        {
-                            new Dictionary<string, object>
-                            {
-                                { "name", "Mimir" },
-                                { "type", "prometheus" },
-                                { "uid", "mimir" },
-                                { "orgId", 1 },
-                                { "url", "http://mimir-distributed-query-frontend:8080/prometheus" },
-                                {
-                                    "jsonData", new Dictionary<string, object>
-                                    {
-                                        ["httpHeaderName1"] = "X-Scope-OrgID"
-                                    }
-                                },
-                                {
-                                    "secureJsonData", new Dictionary<string, object>
-                                    {
-                                        ["httpHeaderValue1"] = "1"
-                                    }
-                                },
-                                { "basicAuth", false },
-                                { "isDefault", true },
-                                { "version", 1 },
-                                { "editable", false }
-                            },
-                            new Dictionary<string, object>
-                            {
-                                { "name", "Tempo" },
-                                { "type", "tempo" },
-//                                {"access", "proxy"},
-                                { "orgId", 1 },
-                                { "url", "http://tempo-distributed-query-frontend:3100" },
-                                { "basicAuth", false },
-                                { "isDefault", false },
-                                { "version", 1 },
-                                { "editable", false },
-                                { "apiVersion", 1 },
-                                { "uid", "tempo" },
-                                {
-                                    "jsonData", new Dictionary<string, object>
-                                    {
-                                        ["tracesToLogsV2"] = new InputMap<object>
-                                        {
-                                            ["datasourceUid"] = "loki",
-                                        },
-                                        ["tracesToMetrics"] = new InputMap<object>
-                                        {
-                                            ["datasourceUid"] = "mimir"
-                                        },
-                                        ["lokiSearch"] = new InputMap<object>
-                                        {
-                                            ["datasourceUid"] = "loki"
-                                        }
-                                    }
-                                }
-                            },
-                            new Dictionary<string, object>
-                            {
-                                { "name", "Loki" },
-                                { "type", "loki" },
-                                { "uid", "loki" },
-                                { "orgId", 1 },
-                                { "url", "http://loki-distributed-query-frontend:3100" },
-                                { "basicAuth", false },
-                                { "isDefault", false },
-                                { "version", 1 },
-                                { "editable", false },
-                                {
-                                    "jsonData", new Dictionary<string, object>
-                                    {
-                                        ["derivedFields"] = new List<object>
-                                        {
-                                            new Dictionary<string, object>
-                                            {
-                                                { "datasourceUid", "tempo" },
-                                                { "matcherRegex", "\"TraceID\":\"(\\w+)" },
-                                                { "name", "TraceID" },
-                                                { "url", "$${__value.raw}" }
-                                            }
-                                        }
-                                    }
-                                }
-                            },
-                            new Dictionary<string, object>
-                            {
-                                { "name", "Pyroscope" },
-                                { "type", "grafana-pyroscope-datasource" },
-                                { "uid", "pyroscope" },
-                                { "orgId", 1 },
-                                { "url", "http://pyroscope-querier.shared.svc.cluster.local.:4040/" },
-                                { "basicAuth", false },
-                                { "isDefault", false },
-                                { "version", 1 },
-                                { "editable", true }
-                            }
-                        }
-                    }
-                }
-            };
-
             string testDashboardJsonString;
             using (var sr = new StreamReader("Component/Shared/Observability/Grafana/Dashboard/dashboard.json"))
             {
@@ -207,6 +50,13 @@ namespace Infrastructure.Pulumi.Component.Shared.Observability.Grafana
                 }
             });
 
+            // ref: https://github.com/grafana/helm-charts/blob/main/charts/grafana/values.yaml
+            var namespaceName = string.Empty; 
+            input.Namespace.Metadata.Apply(x =>
+            {
+                namespaceName = x.Name;
+                return x;
+            });
             var grafana = new Release("grafana", new ReleaseArgs
             {
                 Name = "grafana",
@@ -218,9 +68,161 @@ namespace Infrastructure.Pulumi.Component.Shared.Observability.Grafana
                     Repo = "https://grafana.github.io/helm-charts"
                 },
                 Atomic = true,
-                Values = values,
+                Values = new Dictionary<string, object>
+                {
+                    ["adminPassword"] = "grafana12345",
+                    ["sidecar"] = new Dictionary<string, object>
+                    {
+                        ["dashboards"] = new Dictionary<string, object>
+                        {
+                            ["enabled"] = true,
+                            ["label"] = "grafana_dashboard",
+                            ["labelValue"] = bool.TrueString.ToLower(),
+                            ["searchNamespace"] =
+                                string.Join(",", namespaceName, _config.GetWebApplicationConfig().Namespace)
+                        }
+                    },
+                    ["dashboardProviders"] = new Dictionary<string, object>
+                    {
+                        ["tidb-dashboard.yaml"] = new Dictionary<string, object>
+                        {
+                            ["apiVersion"] = 1,
+                            ["providers"] = new List<object>
+                            {
+                                new Dictionary<string, object>
+                                {
+                                    ["folder"] = "",
+                                    ["name"] = "0",
+                                    ["options"] = new Dictionary<string, object>
+                                    {
+                                        ["path"] = "/grafana-dashboard-definitions/tidb"
+                                    },
+                                    ["allowUiUpdates"] = bool.TrueString.ToLower(),
+                                    ["orgId"] = 1,
+                                    ["type"] = "file"
+                                }
+                            }
+                        }
+                    },
+                    ["extraVolumeMounts"] = new List<object>
+                    {
+                        new Dictionary<string, object>
+                        {
+                            ["name"] = "tidb-grafana-dashboard",
+                            ["mountPath"] = "/grafana-dashboard-definitions/tidb"
+                        }
+                    },
+                    ["datasources"] = new Dictionary<string, object>
+                    {
+                        ["datasource.yaml"] = new Dictionary<string, object>
+                        {
+                            ["apiVersion"] = 1,
+                            ["datasources"] = new List<object>
+                            {
+                                new Dictionary<string, object>
+                                {
+                                    { "name", "Mimir" },
+                                    { "type", "prometheus" },
+                                    { "uid", "mimir" },
+                                    { "orgId", 1 },
+                                    { "url", "http://mimir-distributed-query-frontend:8080/prometheus" },
+                                    {
+                                        "jsonData", new Dictionary<string, object>
+                                        {
+                                            ["httpHeaderName1"] = "X-Scope-OrgID"
+                                        }
+                                    },
+                                    {
+                                        "secureJsonData", new Dictionary<string, object>
+                                        {
+                                            ["httpHeaderValue1"] = "1"
+                                        }
+                                    },
+                                    { "basicAuth", false },
+                                    { "isDefault", true },
+                                    { "version", 1 },
+                                    { "editable", false }
+                                },
+                                new Dictionary<string, object>
+                                {
+                                    { "name", "Tempo" },
+                                    { "type", "tempo" },
+//                                {"access", "proxy"},
+                                    { "orgId", 1 },
+                                    { "url", "http://tempo-distributed-query-frontend:3100" },
+                                    { "basicAuth", false },
+                                    { "isDefault", false },
+                                    { "version", 1 },
+                                    { "editable", false },
+                                    { "apiVersion", 1 },
+                                    { "uid", "tempo" },
+                                    {
+                                        "jsonData", new Dictionary<string, object>
+                                        {
+                                            ["tracesToLogsV2"] = new InputMap<object>
+                                            {
+                                                ["datasourceUid"] = "loki",
+                                            },
+                                            ["tracesToMetrics"] = new InputMap<object>
+                                            {
+                                                ["datasourceUid"] = "mimir"
+                                            },
+                                            ["lokiSearch"] = new InputMap<object>
+                                            {
+                                                ["datasourceUid"] = "loki"
+                                            }
+                                        }
+                                    }
+                                },
+                                new Dictionary<string, object>
+                                {
+                                    { "name", "Loki" },
+                                    { "type", "loki" },
+                                    { "uid", "loki" },
+                                    { "orgId", 1 },
+                                    { "url", "http://loki-distributed-query-frontend:3100" },
+                                    { "basicAuth", false },
+                                    { "isDefault", false },
+                                    { "version", 1 },
+                                    { "editable", false },
+                                    {
+                                        "jsonData", new Dictionary<string, object>
+                                        {
+                                            ["derivedFields"] = new List<object>
+                                            {
+                                                new Dictionary<string, object>
+                                                {
+                                                    { "datasourceUid", "tempo" },
+                                                    { "matcherRegex", "\"TraceID\":\"(\\w+)" },
+                                                    { "name", "TraceID" },
+                                                    { "url", "$${__value.raw}" }
+                                                }
+                                            }
+                                        }
+                                    }
+                                },
+                                new Dictionary<string, object>
+                                {
+                                    { "name", "Pyroscope" },
+                                    { "type", "grafana-pyroscope-datasource" },
+                                    { "uid", "pyroscope" },
+                                    { "orgId", 1 },
+                                    { "url", "http://pyroscope-querier.shared.svc.cluster.local.:4040/" },
+                                    { "basicAuth", false },
+                                    { "isDefault", false },
+                                    { "version", 1 },
+                                    { "editable", true }
+                                }
+                            }
+                        }
+                    }
+                },
                 RecreatePods = true,
                 Namespace = input.Namespace.Metadata.Apply(x => x.Name)
+            },
+            new CustomResourceOptions
+            {
+                DependsOn = input.Namespace
             });
 
             var ingress = new Ingress("grafana-ingress", new IngressArgs
@@ -236,7 +238,7 @@ namespace Infrastructure.Pulumi.Component.Shared.Observability.Grafana
                     IngressClassName = "nginx",
                     Rules = new List<IngressRuleArgs>
                     {
-                        new IngressRuleArgs
+                        new()
                         {
                             Host = "grafana.o11y.test",
                             Http = new HTTPIngressRuleValueArgs
--- src/Infrastructure.Pulumi/Component/Shared/Storage/TiDB/TiDBComponentApplyArgs.cs
+++ src/Infrastructure.Pulumi/Component/Shared/Storage/TiDB/TiDBComponentApplyArgs.cs
@@ -4,7 +4,7 @@ namespace Infrastructure.Pulumi.Component.Shared.Storage.TiDB
 {
     public sealed class TiDBComponentInput : IComponentInput
     {
-        public Namespace Namespace { get; set; } = null!;
+        public Namespace Namespace { get; init; } = null!;
     }
 
     public sealed class TiDBComponentOutput : IComponentOutput
