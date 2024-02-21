5daa1082ebc51e55d86ee4bb685dd97e8d670966
konnta0 <konnta0@gmail.com>
2024-02-22 00:41:01 +0900

    use primary constructor

--- src/WebApplication/WebApplication.Infrastructure/Core/RequestHandler/RepositoryHandler.cs
+++ src/WebApplication/WebApplication.Infrastructure/Core/RequestHandler/RepositoryHandler.cs
@@ -6,41 +6,30 @@ using WebApplication.Infrastructure.Repository;
 
 namespace WebApplication.Infrastructure.Core.RequestHandler;
 
-public class RepositoryHandler : IRepositoryHandler
+public class RepositoryHandler(
+    IAsyncRequestAllHandler<IRepositoryInput, IRepositoryOutput?> asyncRequestHandler,
+    IRequestAllHandler<IRepositoryInput, IRepositoryOutput?> requestHandler,
+    IRepositoryActivityStarter activityStarter,
+    IRepositoryInputTypeResolver repositoryInputTypeResolver)
+    : IRepositoryHandler
 {
-    private readonly IAsyncRequestAllHandler<IRepositoryInput, IRepositoryOutput?> _asyncRequestHandler;
-    private readonly IRequestAllHandler<IRepositoryInput, IRepositoryOutput?> _requestHandler;
-    private readonly IRepositoryActivityStarter _activityStarter;
-    private readonly IRepositoryInputTypeResolver _repositoryInputTypeResolver;
-    
-    public RepositoryHandler(
-        IAsyncRequestAllHandler<IRepositoryInput, IRepositoryOutput?> asyncRequestHandler, 
-        IRequestAllHandler<IRepositoryInput, IRepositoryOutput?> requestHandler, 
-        IRepositoryActivityStarter activityStarter, IRepositoryInputTypeResolver repositoryInputTypeResolver)
-    {
-        _asyncRequestHandler = asyncRequestHandler;
-        _requestHandler = requestHandler;
-        _activityStarter = activityStarter;
-        _repositoryInputTypeResolver = repositoryInputTypeResolver;
-    }
-
     public async ValueTask<TOutput> InvokeAsync<TInput, TOutput>(Action<TInput> inputFunc, CancellationToken cancellationToken = new ()) where TInput : IRepositoryInput where TOutput : IRepositoryOutput
     {
-        var inputType = _repositoryInputTypeResolver.Resolve<TInput>() ?? throw new InvalidOperationException($"Not found {typeof(TInput).Name}.");
+        var inputType = repositoryInputTypeResolver.Resolve<TInput>() ?? throw new InvalidOperationException($"Not found {typeof(TInput).Name}.");
         var input = (TInput)Activator.CreateInstance(inputType)! ?? throw new InvalidOperationException($"Not found {typeof(TInput).Name}.");
         inputFunc.Invoke(input);
         
-        using var activity = _activityStarter.Start();
+        using var activity = activityStarter.Start();
         activity?.SetTag("inputType", typeof(TInput).Name);
         
-        var results = await _asyncRequestHandler.InvokeAllAsync(input, AsyncPublishStrategy.Parallel, cancellationToken);
+        var results = await asyncRequestHandler.InvokeAllAsync(input, AsyncPublishStrategy.Parallel, cancellationToken);
         var result = results.Where(x => x is not null).OfType<TOutput>().FirstOrDefault();
         if (result is not null)
         {
             return result;
         }
         
-        results = _requestHandler.InvokeAll(input);
+        results = requestHandler.InvokeAll(input);
         result = results.Where(x => x is not null).OfType<TOutput>().FirstOrDefault();
 
         if (result is null)
--- src/WebApplication/WebApplication.Infrastructure/Database/TransactionalFlow.cs
+++ src/WebApplication/WebApplication.Infrastructure/Database/TransactionalFlow.cs
@@ -3,24 +3,17 @@ using WebApplication.Infrastructure.Database.Context;
 
 namespace WebApplication.Infrastructure.Database;
 
-public class TransactionalFlow : ITransactionalFlow
+public class TransactionalFlow(IDbContextHolder dbContextHolder) : ITransactionalFlow
 {
-    private readonly IDbContextHolder _dbContextHolder;
-
-    public TransactionalFlow(IDbContextHolder dbContextHolder)
-    {
-        _dbContextHolder = dbContextHolder;
-    }
-    
     public async ValueTask SaveChangesAsync(CancellationToken cancellationToken = default)
     {
-        var dbContexts = _dbContextHolder.GetAll();
+        var dbContexts = dbContextHolder.GetAll();
         await Task.WhenAll(dbContexts.Select(t => t.SaveChangesAsync(false, cancellationToken)).Cast<Task>().ToList());
     }
 
     public void AcceptAllChangesAsync()
     {
-        var dbContexts = _dbContextHolder.GetAll();
+        var dbContexts = dbContextHolder.GetAll();
 
         foreach (var c in dbContexts)
         {
