5f4bf68f6469f5ea6d47eb4115730b23cf5d5b15
konnta0 <konnta0@gmail.com>
2024-02-25 01:28:33 +0900

    chore: start microk8s

--- src/Tool.Make/Microk8s.cs
+++ src/Tool.Make/Microk8s.cs
@@ -2,6 +2,13 @@ namespace Tool.Make;
 
 internal sealed class Microk8s : ConsoleAppBase
 {
+    public async ValueTask Start()
+    {
+        var target = new Targets();
+        target.Add("start", static () => RunAsync("microk8s", "start"));
+        await target.RunWithoutExitingAsync(["start"]);
+    }
+    
     public async ValueTask Stop()
     {
         var target = new Targets();
