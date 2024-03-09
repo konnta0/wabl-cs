fb592c4828d1c6f39cad1d0c3146a577825a0a35
konnta0 <konnta0@gmail.com>
2024-03-09 12:07:30 +0900

    chore

--- src/Tool.LoadTest.RestApi/Program.cs
+++ src/Tool.LoadTest.RestApi/Program.cs
@@ -13,32 +13,67 @@ if (string.IsNullOrEmpty(rootAddress))
     throw new ArgumentException($"Invalid env. CONTROLLER_ROOT_ADDRESS: {rootAddress}");
 }
 
-var workLoadName = Environment.GetEnvironmentVariable("WORKLOAD_NAME");
-if (string.IsNullOrEmpty(workLoadName))
+var workloadName = Environment.GetEnvironmentVariable("WORKLOAD_NAME");
+if (string.IsNullOrEmpty(workloadName))
 {
-    throw new ArgumentException($"Invalid env. WORKLOAD_NAME: {workLoadName}");
+    throw new ArgumentException($"Invalid env. WORKLOAD_NAME: {workloadName}");
 }
 
-var parsed = int.TryParse(Environment.GetEnvironmentVariable("CONCURRENCY"), out var concurrency);
-if (!parsed)
+if (!int.TryParse(Environment.GetEnvironmentVariable("CONCURRENCY"), out var concurrency))
 {
     throw new ArgumentException($"Invalid env. CONCURRENCY: {concurrency}");
 }
 
-parsed = int.TryParse(Environment.GetEnvironmentVariable("TOTAL_REQUEST"), out var totalReqeust);
-if (!parsed)
+if (!int.TryParse(Environment.GetEnvironmentVariable("TOTAL_REQUEST"), out var totalRequest))
 {
-    throw new ArgumentException($"Invalid env. TOTAL_REQUEST: {totalReqeust}");
+    throw new ArgumentException($"Invalid env. TOTAL_REQUEST: {totalRequest}");
 }
 
 var client = new DFrameClient(rootAddress);
 
-await client.ExecuteRequestAsync(new()
+switch (mode.ToLower())
 {
-    Workload = workLoadName,
-    Concurrency = concurrency,
-    TotalRequest = totalReqeust
-});
+    case "request":
+        int? workerLimit = null;
+        if (int.TryParse(Environment.GetEnvironmentVariable("WORKER_LIMIT"), out var limit))
+        {
+            workerLimit = limit;
+        }
+        
+        await client.ExecuteRequestAsync(new()
+        {
+            Workload = workloadName,
+            Concurrency = concurrency,
+            TotalRequest = totalRequest,
+            Workerlimit = workerLimit,
+            Parameters = null
+        });
+        break;
+    case "repeat":
+        var increaseTotalRequest = 0;
+        if (int.TryParse(Environment.GetEnvironmentVariable("INCREASE_TOTAL_REQUEST"), out var r))
+        {
+            increaseTotalRequest = r;
+        }
+
+        var increaseTotalWorker = 0;
+        if (int.TryParse(Environment.GetEnvironmentVariable("INCREASE_TOTAL_WORKER"), out var w))
+        {
+            increaseTotalWorker = w;
+        }
+
+        await client.ExecuteRepeatAsync(new()
+        {
+            Workload = workloadName,
+            Concurrency = concurrency,
+            TotalRequest = totalRequest,
+            Parameters = null,
+            IncreaseTotalRequest = increaseTotalRequest,
+            IncreaseTotalWorker = increaseTotalWorker,
+        });
+        break;
+}
+
 
 await client.WaitUntilCanExecute();
 
