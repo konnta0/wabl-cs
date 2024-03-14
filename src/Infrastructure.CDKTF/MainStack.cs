360fcf70999a88bac56e928c92b9045ce968c403
konnta0 <konnta0@gmail.com>
2024-03-15 02:01:44 +0900

    feat: create namespace

--- src/Infrastructure.CDKTF/MainStack.cs
+++ src/Infrastructure.CDKTF/MainStack.cs
@@ -1,5 +1,7 @@
 using Constructs;
 using HashiCorp.Cdktf;
+using HashiCorp.Cdktf.Providers.Kubernetes.Namespace;
+using HashiCorp.Cdktf.Providers.Kubernetes.Provider;
 
 namespace Infrastructure.CDKTF;
 
@@ -7,6 +9,30 @@ class MainStack : TerraformStack
 {
     public MainStack(Construct scope, string id) : base(scope, id)
     {
-        // define resources here
+        _ = new KubernetesProvider(this, "k8s", new KubernetesProviderConfig());
+
+        var sharedNamespace = new Namespace(this, "namespace-shared", new NamespaceConfig
+        {
+            Metadata = new NamespaceMetadata
+            {
+                Name = "shared"
+            }
+        });
+
+        var toolNamespace = new Namespace(this, "namespace-tool", new NamespaceConfig
+        {
+            Metadata = new NamespaceMetadata
+            {
+                Name = "tool"
+            }
+        });
+
+        var webappNamespace = new Namespace(this, "namespace-webapp", new NamespaceConfig
+        {
+            Metadata = new NamespaceMetadata
+            {
+                Name = "webapp"
+            }
+        });
     }
 }
