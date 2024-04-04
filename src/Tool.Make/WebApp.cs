ae343c7776d883f2a0be3e242319d64af6c61ba5
konnta0 <konnta0@gmail.com>
2024-04-05 00:57:02 +0900

    build tag etc

--- src/Tool.Make/WebApp.cs
+++ src/Tool.Make/WebApp.cs
@@ -1,32 +1,38 @@
 namespace Tool.Make;
 
+/// <summary>
+/// Build and push web app image
+/// When you run these command in out of the kubenetes cluster and using the microk8s's internal registry, you need to set insecure-registries in /var/snap/microk8s/current/args/containerd-template.toml (or ~/.docker/daemon.json)
+/// </summary>
 public sealed class WebApp : ConsoleAppBase
 {
     private readonly Targets _target;
 
+    private const string DockerRegistryHost = "192.168.105.27:32000";
+    
     public WebApp()
     {
         _target = new Targets();
     }
 
     [Command("build-image", "build web image")]
-    public Task Build([Option("t")] string[]? tags = null)
+    public Task Build([Option("t")] string[]? tags = null, [Option("h")] string host = DockerRegistryHost)
     {
         tags ??= ["latest"];
-        _target.Add("build-image", () => RunAsync("docker", $"build {GetBuildTags(tags)} ../../../"));
+        _target.Add("build-image", () => RunAsync("docker", $"build {GetBuildTags(host, tags)} ../../../"));
 
         return _target.RunWithoutExitingAsync(["build"]);
     }
 
-    private string GetBuildTags(string[] tags) => string.Join(" ", tags.Select(static x => $"-t core.harbor.cr.test/webapp/web-api:{x}"));
-    
+    private string GetBuildTags(string host, string[] tags) => string.Join(" ", tags.Select(x => $"-t {host}/webapp/web-api:{x}"));
+
     [Command("push-image", "push web image")]
-    public Task Push([Option("t")] string[]? tags = null)
+    public Task Push([Option("t")] string[]? tags = null, [Option("h")] string host = DockerRegistryHost)
     {
         tags ??= ["latest"];
-        _target.Add("build-image", () => RunAsync("docker", $"build {GetBuildTags(tags)} ../../../"));
+        _target.Add("build-image", () => RunAsync("docker", $"build {GetBuildTags(host, tags)} ../../../"));
         _target.Add("push-image", DependsOn("build"),
-            () => RunAsync("docker", "push -a core.harbor.cr.test/webapp/web-api"));
+            () => RunAsync("docker", $"push -a {DockerRegistryHost}/webapp/web-api"));
 
         return _target.RunWithoutExitingAsync(["push"]);
     }
