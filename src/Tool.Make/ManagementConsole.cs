8e398e7b8d25e884abee06ddba54fd9ee2907f74
konnta0 <konnta0@gmail.com>
2024-05-05 01:23:25 +0900

    feat: management console commands

---
+++ src/Tool.Make/ManagementConsole.cs
@@ -0,0 +1,41 @@
+namespace Tool.Make;
+
+/// <summary>
+/// Build and push management console image
+/// When you run these command in out of the kubenetes cluster and using the microk8s's internal registry, you need to set insecure-registries in /var/snap/microk8s/current/args/containerd-template.toml (or ~/.docker/daemon.json)
+/// </summary>
+internal sealed class ManagementConsole : ConsoleAppBase
+{
+    private readonly Targets _target = new();
+
+    private const string DockerRegistryHost = "192.168.112.5:32000";
+
+    [Command("build-image", "build management consle image")]
+    public Task Build([Option("t")] string[]? tags = null, [Option("h")] string host = DockerRegistryHost)
+    {
+        tags ??= ["latest"];
+        _target.Add("build-image", () => RunAsync("docker", $"buildx build {GetBuildTags(host, tags)} -f ../../Dockerfile.ManagementConsole ../../"));
+
+        return _target.RunWithoutExitingAsync(["build-image"]);
+    }
+
+    private string GetBuildTags(string host, string[] tags) => string.Join(" ", tags.Select(x => $"-t {host}/webapp/management-console:{x}"));
+
+    [Command("push-image", "push management console image")]
+    public Task Push([Option("t")] string[]? tags = null, [Option("h")] string host = DockerRegistryHost)
+    {
+        tags ??= ["latest"];
+        _target.Add("build-and-push-image", () => RunAsync("docker", $"buildx build {GetBuildTags(host, tags)} -f ../../Dockerfile.ManagementConsole ../../ --push"));
+
+        return _target.RunWithoutExitingAsync(["build-and-push-image"]);
+    }
+    
+    [Command("deploy", "deploy management console")]
+    public async Task Deploy(
+        [Option("s")] string stack, 
+        [Option("t")] string? tag = "latest")
+    {
+        await $"pulumi config set --stack {stack} --cwd ../Infrastructure.Pulumi --path 'ManagementConsole.Tag' {tag}";
+        await $"pulumi up --stack {stack} --yes --target **tool-management-console-deployment** --target-dependents --disable-integrity-checking --cwd ../Infrastructure.Pulumi";
+    }
+}
