1643c217b3c1337d4612876b1b84042631e81518
konnta0 <konnta0@gmail.com>
2024-05-28 00:25:55 +0900

    refactor: use primary constructor

--- src/WebApplication/WebApplication.Infrastructure/Core/HealthCheck/ApiHealthCheck.cs
+++ src/WebApplication/WebApplication.Infrastructure/Core/HealthCheck/ApiHealthCheck.cs
@@ -3,27 +3,20 @@ using WebApplication.Infrastructure.Core.Instrumentation.UseCase.Meter;
 
 namespace WebApplication.Infrastructure.Core.HealthCheck;
 
-internal class ApiHealthCheck : IHealthCheck
+internal class ApiHealthCheck(IUseCaseInstrumentationMeter useCaseInstrumentationMeter) : IHealthCheck
 {
-    private readonly IUseCaseInstrumentationMeter _useCaseInstrumentationMeter;
-
-    public ApiHealthCheck(IUseCaseInstrumentationMeter useCaseInstrumentationMeter)
-    {
-        _useCaseInstrumentationMeter = useCaseInstrumentationMeter;
-    }
-
     public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new())
     {
         var isHealthy = true;
-        
+
         if (isHealthy)
         {
-            _useCaseInstrumentationMeter.HealthCheckCounter.Add(1, new KeyValuePair<string, object?>("healthy", bool.TrueString));
+            useCaseInstrumentationMeter.HealthCheckCounter.Add(1, new KeyValuePair<string, object?>("healthy", bool.TrueString));
             return Task.FromResult(
                 HealthCheckResult.Healthy("A healthy result."));
         }
 
-        _useCaseInstrumentationMeter.HealthCheckCounter.Add(1, new KeyValuePair<string, object?>("healthy", bool.FalseString));
+        useCaseInstrumentationMeter.HealthCheckCounter.Add(1, new KeyValuePair<string, object?>("healthy", bool.FalseString));
         return Task.FromResult(
             new HealthCheckResult(
                 context.Registration.FailureStatus, "An unhealthy result."));
