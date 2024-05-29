7ab5339065380f9ed7008c49b8ade2f24327910a
konnta0 <konnta0@gmail.com>
2024-05-30 00:51:30 +0900

    update Version

--- src/Infrastructure.Pulumi/Component/Shared/CiCd/GitHubActions/GitHubActionsComponentApplyArgs.cs
+++ src/Infrastructure.Pulumi/Component/Shared/CiCd/GitHubActions/GitHubActionsComponentApplyArgs.cs
@@ -6,7 +6,7 @@ namespace Infrastructure.Pulumi.Component.Shared.CiCd.GitHubActions;
 public sealed class GitHubActionsComponentInput : IComponentInput
 {
     public Namespace Namespace { get; init; } = null!;
-    public string Version { get; init; } = "0.8.3";
+    public string Version { get; init; } = "0.9.2";
 }
 
 public sealed class GitHubActionsComponentOutput : IComponentOutput
