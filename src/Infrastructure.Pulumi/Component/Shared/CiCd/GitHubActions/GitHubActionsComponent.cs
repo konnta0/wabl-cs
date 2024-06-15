0b0286d5361351c79fb4e80c16ed59ad38271ff1
konnta0 <konnta0@gmail.com>
2024-06-15 12:36:46 +0900

    add kubernetes mode

--- src/Infrastructure.Pulumi/Component/Shared/CiCd/GitHubActions/GitHubActionsComponent.cs
+++ src/Infrastructure.Pulumi/Component/Shared/CiCd/GitHubActions/GitHubActionsComponent.cs
@@ -34,14 +34,127 @@ public sealed class GitHubActionsComponent(Config config)
             },
             ["containerMode"] = new Dictionary<string, object>
             {
-                ["type"] = ""
+                ["type"] = "" // https://github.com/actions/actions-runner-controller/blob/gha-runner-scale-set-0.8.3/charts/gha-runner-scale-set/values.yaml#L76-L77
             },
+            // for kubernetes mode
+            // ["kubernetesModeWorkVolumeClaim"] = new Dictionary<string, object>
+            // {
+            //     ["accessModes"] = new List<string> { "ReadWriteOnce" },
+            //     ["storageClassName"] = "dynamic-blob-storage",
+            //     ["resources"] = new Dictionary<string, object>
+            //     {
+            //         ["requests"] = new Dictionary<string, object>
+            //         {
+            //             ["storage"] = "1Gi"
+            //         }
+            //     }
+            // },
             ["controllerServiceAccount"] = new Dictionary<string, object>
             {
                 ["namespace"] = input.Namespace.Metadata.Apply(static x => x.Name),
                 ["name"] = controller.Name.Apply(static x => x + "-gha-rs-controller")
             },
             ["minRunners"] = 1,
+            // for kubernetes mode
+            // ["template"] = new InputMap<object>
+            // {
+            //     ["spec"] = new InputMap<object>
+            //     {
+            //         { 
+            //             "containers", new InputList<InputMap<object>>
+            //             {
+            //                 new InputMap<object>
+            //                 {
+            //                     { "name", "runner" },
+            //                     { "image", "ghcr.io/actions/actions-runner:latest" },
+            //                     {
+            //                         "command",
+            //                         new InputList<string>
+            //                             { "/home/runner/run.sh" }
+            //                     },
+            //                     {
+            //                         "env", new InputList<InputMap<object>>
+            //                         {
+            //                             new InputMap<object>
+            //                             {
+            //                                 { "name", "ACTIONS_RUNNER_CONTAINER_HOOKS" },
+            //                                 { "value", "/home/runner/k8s/index.js" },
+            //                             },
+            //                             new InputMap<object>
+            //                             {
+            //                                 { "name", "ACTIONS_RUNNER_POD_NAME" },
+            //                                 { 
+            //                                     "valueFrom", new InputMap<object>
+            //                                     {
+            //                                         { "fieldRef", new InputMap<object>
+            //                                             {
+            //                                                 { "fieldPath", "metadata.name" } 
+            //                                                 
+            //                                             }
+            //                                         }
+            //                                     }
+            //                                 }
+            //                             },
+            //                             new InputMap<object>
+            //                             {
+            //                                 { "name", "ACTIONS_RUNNER_REQUIRE_JOB_CONTAINER" },
+            //                                 { "value", "false"}
+            //                             }
+            //                         }
+            //                     },
+            //                     {
+            //                         "volumeMounts", new InputList<InputMap<object>>
+            //                         {
+            //                             new InputMap<object>
+            //                             {
+            //                                 { "name", "work" },
+            //                                 { "mountPath", "/home/runner/_work" }
+            //                             }
+            //                         }
+            //                     }
+            //                 }
+            //             }
+            //         },
+            //         {
+            //             "volumes", new InputList<InputMap<object>>
+            //             {
+            //                 new InputMap<object>
+            //                 {
+            //                     { "name", "work" },
+            //                     {
+            //                         "ephemeral", new InputMap<object>
+            //                         {
+            //                             {
+            //                                 "volumeClaimTemplate", new InputMap<object>
+            //                                 {
+            //                                     {
+            //                                         "spec", new InputMap<object>
+            //                                         {
+            //                                             { "accessModes", new List<string> { "ReadWriteOnce" } },
+            //                                             { "storageClassName", "local-path" },
+            //                                             {
+            //                                                 "resources", new InputMap<object>
+            //                                                 {
+            //                                                     {
+            //                                                         "requests", new InputMap<object>
+            //                                                         {
+            //                                                             { "storage", "1Gi" }
+            //                                                         }
+            //                                                     }
+            //                                                 }
+            //                                             }
+            //                                         }
+            //                                     }
+            //                                 }
+            //                             }
+            //                         }
+            //                     }
+            //                 }
+            //             }
+            //         }
+            //     }
+            // }
+            // for DinD mode
             ["template"] = new InputMap<object>
             {
                 ["spec"] = new InputMap<object>
