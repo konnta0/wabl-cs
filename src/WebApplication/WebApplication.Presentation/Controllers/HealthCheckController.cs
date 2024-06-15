0bd1e0d3d6cd863b1827450990ea48b1ead63644
konnta0 <konnta0@gmail.com>
2024-06-16 01:25:26 +0900

    wip

--- src/WebApplication/WebApplication.Presentation/Controllers/HealthCheckController.cs
+++ src/WebApplication/WebApplication.Presentation/Controllers/HealthCheckController.cs
@@ -1,16 +1,31 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication.Application.Core.RequestHandler;
+using WebApplication.Application.UseCase.Departments.Dto;
 using WebApplication.Presentation.Core;
+using WebApplication.Presentation.Extension.ResponseDataFactory.Departments;
+using WebApplication.Presentation.Extension.ResponseDataFactory.HealthChecks;
 using ZLogger;
 
 namespace WebApplication.Presentation.Controllers;
 
 [ApiController]
-public class HealthCheckController(ILogger<HealthCheckController> logger) : WebApiController
+public class HealthCheckController(ILogger<HealthCheckController> logger, IUseCaseHandler useCaseHandler) : WebApiController
 {
     [HttpGet]
     public IActionResult Ping()
     {
         logger.ZLogInformation("Called ping");
         return new JsonResult("pong") { StatusCode = 200 };
+    }
+
+    [AllowAnonymous]
+    [HttpGet]
+    public async ValueTask<IActionResult> System()
+    {
+        var input = new GetSystemInfoUseCaseInput();
+        var output = await useCaseHandler.InvokeAsync<GetSystemInfoUseCaseInput, GetSystemInfoUseCaseOutput>(input);
+        var response = GetSystemInfoFactory.Create(output);
+        return new JsonResult(response) { StatusCode = 200 };
     }
 }
