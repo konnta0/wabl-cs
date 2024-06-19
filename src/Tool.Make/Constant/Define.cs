d611cc6f5f23ecb91d6e9559d4c3d6b8fd5de928
konnta0 <konnta0@gmail.com>
2024-06-20 00:46:40 +0900

    change host

--- src/Tool.Make/Constant/Define.cs
+++ src/Tool.Make/Constant/Define.cs
@@ -2,5 +2,5 @@ namespace Tool.Make.Constant;
 
 internal static class Define
 {
-    public const string ContainerRegistryHost = "k3d-wabl-cs-registry:58063";
+    public const string ContainerRegistryHost = "k3d-wabl-cs-registry.localhost:5000";
 }
