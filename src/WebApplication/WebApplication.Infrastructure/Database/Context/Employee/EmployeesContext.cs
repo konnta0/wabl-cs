fc395b3a0220578dfbef284c8b296b166daed0ef
konnta0 <konnta0@gmail.com>
2024-06-26 00:43:27 +0900

    change database name

--- src/WebApplication/WebApplication.Infrastructure/Database/Context/Employee/EmployeesContext.cs
+++ src/WebApplication/WebApplication.Infrastructure/Database/Context/Employee/EmployeesContext.cs
@@ -2,12 +2,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication.Infrastructure.Database.Context.Employee;
 
-public partial class EmployeesContext : DbContext
+public partial class EmployeesContext(DbContextOptions<EmployeesContext> dbContextOptions) : DbContext(dbContextOptions)
 {
-    public EmployeesContext(DbContextOptions<EmployeesContext> dbContextOptions) : base(dbContextOptions)
-    {
-    }
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         new EmployeesContextOnModelCreatingDispatcher().Invoke(modelBuilder);
@@ -15,6 +11,6 @@ public partial class EmployeesContext : DbContext
 
     public static string GetConnectionString(DatabaseConfig databaseConfig)
     {
-        return $"server={databaseConfig.ServerHost};port={databaseConfig.ServerPort};user={databaseConfig.ServerUser};password={databaseConfig.ServerPassword};Database=employees";
+        return $"server={databaseConfig.ServerHost};port={databaseConfig.ServerPort};user={databaseConfig.ServerUser};password={databaseConfig.ServerPassword};Database=app_db";
     }
 }
