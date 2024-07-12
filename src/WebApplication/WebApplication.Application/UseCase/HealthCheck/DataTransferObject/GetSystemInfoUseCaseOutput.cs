2b3978cc6a0a16fbe7b1c7d64a73f05c307d21ba
konnta0 <konnta0@gmail.com>
2024-07-13 04:24:38 +0900

    refactor: add required

--- src/WebApplication/WebApplication.Application/UseCase/HealthCheck/DataTransferObject/GetSystemInfoUseCaseOutput.cs
+++ src/WebApplication/WebApplication.Application/UseCase/HealthCheck/DataTransferObject/GetSystemInfoUseCaseOutput.cs
@@ -2,7 +2,7 @@ namespace WebApplication.Application.UseCase.HealthCheck.DataTransferObject;
 
 public sealed class GetSystemInfoUseCaseOutput : IGetSystemInfoUseCaseOutput
 {
-    public Cache Durable { get; init; } = new (false);
-    public Cache Volatile { get; init; } = new (false);
-    public Database Database { get; init; } = new(false);
+    public required Cache Durable { get; init; } = new (false);
+    public required Cache Volatile { get; init; } = new (false);
+    public required Database Database { get; init; } = new(false);
 }
