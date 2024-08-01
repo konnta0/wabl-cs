d5b4b8a45843e0c8ebac8beb62cd7a0ee1d6aa04
konnta0 <konnta0@gmail.com>
2024-08-02 00:46:06 +0900

    chore: refactor

--- src/Infrastructure.Pulumi/Component/Shared/Storage/StorageComponent.cs
+++ src/Infrastructure.Pulumi/Component/Shared/Storage/StorageComponent.cs
@@ -7,52 +7,39 @@ using Pulumi;
 
 namespace Infrastructure.Pulumi.Component.Shared.Storage;
 
-public class StorageComponent : IComponent<StorageComponentInput, StorageComponentOutput>
+public class StorageComponent(
+    Config config,
+    MinIoComponent minIoComponent,
+    DragonflyComponent dragonflyComponent,
+    RedisComponent redisComponent,
+    TiDBComponent tiDbComponent)
+    : IComponent<StorageComponentInput, StorageComponentOutput>
 {
-    private readonly Config _config;
-    private readonly MinIoComponent _minIoComponent;
-    private readonly DragonflyComponent _dragonflyComponent;
-    private readonly RedisComponent _redisComponent;
-    private readonly TiDBComponent _tiDbComponent;
-
-    public StorageComponent(Config config, 
-        MinIoComponent minIoComponent, 
-        DragonflyComponent dragonflyComponent, 
-        RedisComponent redisComponent,
-        TiDBComponent tiDbComponent)
-    {
-        _config = config;
-        _minIoComponent = minIoComponent;
-        _dragonflyComponent = dragonflyComponent;
-        _redisComponent = redisComponent;
-        _tiDbComponent = tiDbComponent;
-    }
-
     public StorageComponentOutput Apply(StorageComponentInput input)
     {
-        var minIoOutput = _minIoComponent.Apply(new MinIoComponentInput
+        var minIoOutput = minIoComponent.Apply(new MinIoComponentInput
         {
             Namespace = input.Namespace
         });
 
-        var cache = _config.RequireObject<JsonElement>("Storage").GetProperty("Cache").GetString();
+        var cache = config.RequireObject<JsonElement>("Storage").GetProperty("Cache").GetString();
 
         switch (cache!.ToLower())
         {
             case "dragonfly":
-                _dragonflyComponent.Apply(new DragonflyComponentInput
+                dragonflyComponent.Apply(new DragonflyComponentInput
                 {
                     Namespace = input.Namespace
                 });
                 break;
             case "redis":
-                _redisComponent.Apply(new RedisComponentInput
+                redisComponent.Apply(new RedisComponentInput
                 {
                     Namespace = input.Namespace
                 });
                 break;
         }
-        _tiDbComponent.Apply(new TiDBComponentInput
+        tiDbComponent.Apply(new TiDBComponentInput
         {
             Namespace = input.Namespace
         });
