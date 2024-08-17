58edd9459de3a8df873da52b3ff009f256099bf5
konnta0 <konnta0@gmail.com>
2024-08-18 01:50:46 +0900

    add test project

---
+++ tests/MessageQueueService.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace MessageQueueService.Test;
+
+public sealed class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
