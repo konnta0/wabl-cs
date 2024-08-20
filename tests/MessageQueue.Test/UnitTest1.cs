3a5a1302e8dc51e936e982c3e63f68ff1cda27e3
konnta0 <konnta0@gmail.com>
2024-08-21 00:19:05 +0900

    rename project

--- tests/MessageQueue.Test/UnitTest1.cs
+++ tests/MessageQueue.Test/UnitTest1.cs
@@ -1,12 +1,54 @@
-using Xunit;
+using DotPulsar;
+using DotPulsar.Abstractions;
+using DotPulsar.Extensions;
+using Xunit.Abstractions;
 
-namespace MessageQueueService.Test;
+namespace MessageQueue.Test;
 
-public class UnitTest1
+public class UnitTest1(ITestOutputHelper testOutputHelper)
 {
-    [Fact]
-    public void Test1()
-    {
 
+    [Fact]
+    public async Task Test1()
+    {
+        var cts = new CancellationTokenSource();
+
+        await using var client = PulsarClient.Builder().Build(); // Connecting to pulsar://localhost:6650
+        await using var producer = client.NewProducer(Schema.String)
+            .StateChangedHandler(Monitor)
+            .Topic("persistent://public/default/mytopic")
+            .Create();
+
+        await ProduceMessages(producer, cts.Token);
+        Assert.True(true);
+        return;
+
+        async Task ProduceMessages(IProducer<string> producer, CancellationToken cancellationToken)
+        {
+            var delay = TimeSpan.FromSeconds(1);
+
+            try
+            {
+                
+                for (var i = 0; i < 30; i++)
+                {
+                    var data = $"{i} : {DateTime.UtcNow.ToLongTimeString()}";
+                    _ = await producer.Send(data, cancellationToken);
+                    testOutputHelper.WriteLine($"Sent: {data}");
+                    await Task.Delay(delay, cancellationToken);
+                }
+            }
+            catch
+                (OperationCanceledException) // If not using the cancellationToken, then just dispose the producer and catch ObjectDisposedException instead
+            {
+            }
+        }
+
+        void Monitor(ProducerStateChanged stateChanged)
+        {
+            var topic = stateChanged.Producer.Topic;
+            var state = stateChanged.ProducerState;
+            testOutputHelper.WriteLine($"The producer for topic '{topic}' changed state to '{state}'");
+        }
     }
 }
