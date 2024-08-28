dbb58919ff19ed6e6fb0af3e7ad8ec87d5f90d07
konnta0 <konnta0@gmail.com>
2024-08-29 00:45:27 +0900

    chore add domain

--- src/MessageQueue.Presentation/Program.cs
+++ src/MessageQueue.Presentation/Program.cs
@@ -1,8 +1,10 @@
 using MessageQueue.Application.Extension;
+using MessageQueue.Domain.Extension;
 using MessageQueue.Infrastructure.Extension;
 using MessageQueue.Presentation.Extension;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddDomain();
 builder.Services.AddInfrastructure();
 builder.Services.AddApplication();
 builder.Services.AddPresentation();
