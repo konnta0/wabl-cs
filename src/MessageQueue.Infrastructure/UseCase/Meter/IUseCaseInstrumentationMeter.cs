80c25d07f4a546f821f952761de5b0fd39198578
konnta0 <konnta0@gmail.com>
2024-09-10 01:18:46 +0900

    refactor

--- src/MessageQueue.Infrastructure/UseCase/Meter/IUseCaseInstrumentationMeter.cs
+++
@@ -1,9 +0,0 @@
-using System.Diagnostics.Metrics;
-using ManagementConsole.Infrastructure.Instrumentation;
-
-namespace MessageQueue.Infrastructure.UseCase.Meter;
-
-public interface IUseCaseInstrumentationMeter : IInstrumentationMeter
-{
-    Counter<int> HealthCheckCounter { get; init; }
-}
--- src/MessageQueue.Infrastructure/UseCase/UseCaseInstrumentationOptions.cs
+++
@@ -1,6 +0,0 @@
-namespace MessageQueue.Infrastructure.UseCase;
-
-public class UseCaseInstrumentationOptions
-{
-    
-}
