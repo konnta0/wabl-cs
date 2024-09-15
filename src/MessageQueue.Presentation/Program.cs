db35554ae6b22b2bf0208182d2f06205e7fe15f1
konnta0 <konnta0@gmail.com>
2024-09-16 01:49:16 +0900

    use UseCase

--- src/MessageQueue.Presentation/Program.cs
+++ src/MessageQueue.Presentation/Program.cs
@@ -1,11 +1,41 @@
+using Cysharp.Text;
 using MessageQueue.Application.Extension;
 using MessageQueue.Domain.Extension;
 using MessageQueue.Infrastructure.Extension;
+using MessageQueue.Infrastructure.Instrumentation;
 using MessageQueue.Presentation.Extension;
+using OpenTelemetry.Logs;
+using Shared.Infrastructure.Cache;
+using Shared.Infrastructure.Logging;
+using ZLogger;
+using ZLogger.Providers;
 
 var builder = Host.CreateApplicationBuilder(args);
+GlobalLogManager.SetLoggerFactory(LoggerFactory.Create(loggingBuilder =>
+{
+    loggingBuilder.ClearProviders();
+    loggingBuilder.SetMinimumLevel(LogLevel.Information);
+    loggingBuilder.AddFilter<ZLoggerConsoleLoggerProvider>("Microsoft", LogLevel.None);
+    loggingBuilder.AddZLoggerConsole(options =>
+    {
+        options.EnableStructuredLogging = true;
+        var prefixFormat = ZString.PrepareUtf8<LogLevel, DateTime>("[{0}][{1}] ");
+        options.PrefixFormatter = (writer, info) =>
+            prefixFormat.FormatTo(ref writer, info.LogLevel, info.Timestamp.DateTime.ToLocalTime());
+    });
+    loggingBuilder.AddOpenTelemetry(options =>
+    {
+        options.IncludeScopes = true;
+        options.ParseStateValues = true;
+        options.IncludeFormattedMessage = true;
+        options.AddInMemoryExporter(new InMemoryLogRecords());
+    });
+}), "Global");
+
+builder.Configuration.Bind(nameof(CacheConfig),new CacheConfig());
+
 builder.Services.AddDomain();
-builder.Services.AddInfrastructure();
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddPresentation();
 
--- src/MessageQueue.Presentation/Worker/KpiLogWorker.cs
+++ src/MessageQueue.Presentation/Worker/KpiLogWorker.cs
@@ -1,66 +1,16 @@
-using System.Text.Json;
-using DotPulsar;
-using DotPulsar.Abstractions;
-using DotPulsar.Extensions;
-using MessageQueue.Domain;
-using MessageQueue.Domain.DataTransferObject;
+using MessageQueue.Application.RequestHandler;
+using MessageQueue.Application.UseCase.KpiLog.DataTransferObject;
 
 namespace MessageQueue.Presentation.Worker;
 
-internal sealed class KpiLogWorker(ILogger<KpiLogWorker> logger) : BackgroundService
+internal sealed class KpiLogWorker(IServiceProvider serviceProvider) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken cancellationToken)
     {
-        await using var client = PulsarClient.Builder()
-            .ExceptionHandler(x =>
-            {
-                logger.LogCritical(x.Exception, "Got Exception. {Result}", x.Result);
-            }) // Optional
-            .Build(); // Connecting to pulsar://localhost:6650
+        using var scope = serviceProvider.CreateScope();
+        var useCaseHandler =
+            scope.ServiceProvider.GetRequiredService<IUseCaseHandler>();
 
-        await using var consumer = client.NewConsumer(CustomSchema.KpiLogSchema)
-            .StateChangedHandler(x =>
-            {
-                logger.LogInformation("Got SateChanged. {Topic}, {Result}", x.Consumer.Topic, x.ConsumerState);
-            }) // Optional
-            .SubscriptionName("KpiLog")
-            .Topic("persistent://public/default/kpilog")
-            .MessagePrefetchCount(100)
-            .Create();
-        
-        _ = consumer.DelayedStateMonitor(  // Recommended way of ignoring the short disconnects expected when working with a distributed system
-            ConsumerState.Active,          // Operational state
-            TimeSpan.FromSeconds(5), // The amount of time allowed in non-operational state before we act
-            (x, state, ctx) =>
-            {
-                logger.LogInformation("Got onStateLeft. {Topic}, {Result}", x.Topic, state);
-                return ValueTask.CompletedTask;
-            },     // Invoked if we are NOT back in operational state after 5 seconds
-            (x, state, ctx) =>
-            {
-                logger.LogInformation("Got onStateReached. {Topic}, {Result}", x.Topic, state);
-                return ValueTask.CompletedTask;
-            }, // Invoked when we are in operational state again
-            cancellationToken);
-
-        const int capacity = 5;
-        List<IMessage<KpiLog>> messages = new(capacity);
-        while (!cancellationToken.IsCancellationRequested)
-        {
-            var message = await consumer.Receive(cancellationToken);
-            
-            messages.Add(message);
-
-            if (messages.Count is not capacity) continue;
-
-            foreach (var msg in messages)
-            {
-                var publishedOn = msg.PublishTimeAsDateTime;
-                var payload = msg.Value();
-                logger.LogInformation("{PublishedOn}: {LogType}, {}", publishedOn, payload.LogType, JsonSerializer.Serialize(payload.Message));
-            }
-            await consumer.AcknowledgeCumulative(messages.Last(), cancellationToken);
-            messages.Clear();
-        }
+        _ = await useCaseHandler.InvokeAsync<AddKpiLogUseCaseInput, AddKpiLogUseCaseOutput>(new (), cancellationToken);
     }
 }
