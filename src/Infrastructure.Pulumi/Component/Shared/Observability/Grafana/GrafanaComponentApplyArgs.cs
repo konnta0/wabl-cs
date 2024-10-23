1cf5ff2285c7c5db512adf9766e0c2004d882384
konnta0 <konnta0@gmail.com>
2024-10-24 03:24:38 +0900

    chore

--- src/Infrastructure.Pulumi/Component/Shared/Observability/Grafana/GrafanaComponentApplyArgs.cs
+++ src/Infrastructure.Pulumi/Component/Shared/Observability/Grafana/GrafanaComponentApplyArgs.cs
@@ -4,7 +4,7 @@ namespace Infrastructure.Pulumi.Component.Shared.Observability.Grafana
 {
     public sealed class GrafanaComponentInput : IComponentInput
     {
-        public Namespace Namespace { get; set; } = null!;
+        public required Namespace Namespace { get; set; }
     }
 
     public sealed class GrafanaComponentOutput : IComponentOutput
